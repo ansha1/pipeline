6af6351ee8200dabe68a2a73ceac02788800e24d
Evgeniy Sakhnyk <Evgeniy.Sakhnyk@nextiva.com>
2018-02-28 17:58:52 +0200

    add notify method

--- vars/notify.groovy
+++ vars/notify.groovy
@@ -7,5 +7,5 @@ def call(String slackChannel = '#devops_kyiv') {
     def commitinfo = sh returnStdout: true, script: 'git show --pretty=format:"The author of %h was %an, %ar%nThe title was >>%s<<%n" | sed -n 1,2p'
     def details = "Check console output at ${env.BUILD_URL}console\n" + commitinfo + "\n" + currentBuild.fullDisplayName + "\n" +
             "Test results: ${env.BUILD_URL}testReport"
-    slackSend channel: slackChannel, color: notifyColor.get(buildStatus), message: subject + ' ' + details, tokenCredentialId: 'slackToken'
+    slackSend channel: '@evgeniy.sakhnyuk', color: notifyColor.get(buildStatus), message: subject + ' ' + details, tokenCredentialId: 'slackToken'
 }
