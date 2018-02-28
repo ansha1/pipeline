84cf1585b748de82ff9a0e1edd0fb6e56079d461
Evgeniy Sakhnyk <Evgeniy.Sakhnyk@nextiva.com>
2018-02-28 18:07:01 +0200

    add notify method

--- vars/notify.groovy
+++ vars/notify.groovy
@@ -4,8 +4,7 @@ def call(String slackChannel = '@evgeniy.sakhnyuk') {
     notifyColor = ['SUCCESS': '#00FF00', 'FAILURE': '#FF0000', 'UNSTABLE': '#FF0000']
 
     def subject = "${buildStatus}: Job '${env.JOB_NAME}' #${env.BUILD_NUMBER}"
-//    def commitinfo = sh returnStdout: true, script: 'git show --pretty=format:"The author of %h was %an, %ar%nThe title was >>%s<<%n" | sed -n 1,2p'
-    def commitinfo = env.CHANGE_AUTHOR_DISPLAY_NAME + env.CHANGE_AUTHOR_EMAIL + env.CHANGE_TARGET + env.CHANGE_URL + env.CHANGE_TITLE + env.CHANGE_AUTHOR
+    def commitinfo = sh returnStdout: true, script: "git show --pretty=format:'The author of %h was %an, %ar%nThe title was >>%s<<%n' | sed -n 1,2p"
     def details = "Check console output at ${env.BUILD_URL}console\n" + commitinfo + "\n" + currentBuild.fullDisplayName + "\n" +
             "Test results: ${env.BUILD_URL}testReport"
     slackSend channel: slackChannel, color: notifyColor.get(buildStatus), message: subject + ' ' + details, tokenCredentialId: 'slackToken'
