e5e789c385240c09ef97820d15d683c57e2fcde0
Evgeniy Sakhnyk <Evgeniy.Sakhnyk@nextiva.com>
2018-02-28 17:43:12 +0200

    add notify method

--- vars/notify.groovy
+++ vars/notify.groovy
@@ -1,4 +1,4 @@
-def call(String slackChannel= '#devops_kyiv') {
+def call(String slackChannel = '#devops_kyiv') {
     echo '1'
     buildStatus=currentBuild.currentResult
     notifyColor = ['SUCCESS': '#00FF00', 'FAILURE': '#FF0000', 'UNSTABLE': '#FF0000']
