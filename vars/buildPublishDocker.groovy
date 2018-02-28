ae4cb327ae801d40fb128274f69732a56c74c733
Evgeniy Sakhnyk <Evgeniy.Sakhnyk@nextiva.com>
2018-02-28 14:34:48 +0200

    add build docker method

--- vars/buildPublishDocker.groovy
+++ vars/buildPublishDocker.groovy
@@ -1,6 +1,6 @@
 def call(String APP_NAME, String BUILD_VERSION, String EXTRA_PATH) {
     docker.withRegistry('https://repository.nextiva.xyz', 'nextivaRegistry') {
-        def customImage = docker.build("${APP_NAME}:${BUILD_VERSION}", "-f ${EXTRA_PATH}/Dockerfile --build-arg build_version=${BUILD_VERSION} ./${EXTRA_PATH}")
+        def customImage = docker.build("${APP_NAME}:${BUILD_VERSION}", "-f ${EXTRA_PATH}/Dockerfile --build-arg build_version=${BUILD_VERSION} .${EXTRA_PATH}")
         customImage.push()
         customImage.push("latest")
 
