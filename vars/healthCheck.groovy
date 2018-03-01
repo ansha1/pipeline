903a25e070c76ff93ebbdcbeb5f31f40149032cb
Evgeniy Sakhnyk <Evgeniy.Sakhnyk@nextiva.com>
2018-03-01 13:41:09 +0200

    add healthcheck method

---
+++ vars/healthCheck.groovy
@@ -0,0 +1,12 @@
+def call(String healthcheckUrl, Integer time = 2) {
+    timeout(time) {   //default 2 minutes for starting
+        waitUntil {
+            try {
+                httpRequest url: healthcheckUrl
+                return true
+            } catch (e) {
+                return false
+            }
+        }
+    }
+}
