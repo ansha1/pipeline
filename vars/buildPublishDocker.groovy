a56eb5a2f01d34b3df0ff837c94852f2f94e61a1
Evgeniy Sakhnyk <Evgeniy.Sakhnyk@nextiva.com>
2018-03-06 18:54:37 +0200

    add cleanup task for publish docker task

--- vars/buildPublishDocker.groovy
+++ vars/buildPublishDocker.groovy
@@ -3,5 +3,7 @@ def call(String APP_NAME, String BUILD_VERSION, String EXTRA_PATH = '.') {
         def customImage = docker.build("${APP_NAME}:${BUILD_VERSION}", "-f ${EXTRA_PATH}/Dockerfile --build-arg build_version=${BUILD_VERSION} ${EXTRA_PATH}")
         customImage.push()
         customImage.push("latest")
+        sh "docker rmi ${customImage.id} repository.nextiva.xyz/${customImage.id}"
+        sh 'docker rmi $(docker images -f "dangling=true" -q)'
     }
 }
