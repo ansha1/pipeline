a5a7ca309e619e8d77f7764842b8dbe57fe30a26
Ivan Fishchuk <ivanfishchuk@Ivans-MacBook-Pro.local>
2018-03-13 16:46:08 +0200

    update archiveToNexus.groovy

--- vars/archiveToNexus.groovy
+++ vars/archiveToNexus.groovy
@@ -1,17 +1,23 @@
-def call(def deployEnvironment, /*def assetDir, def version,*/ def upstreamJobName/*, def packageName*/) {
+def call(def deployEnvironment, def assetDir, def version, def upstreamJobName, def packageName) {
+  
 
    def assetPath = "${env.EXECUTOR_NUMBER}" + '.bzip'
    def jobName = upstreamJobName ?: "${env.JOB_NAME}"
    def listOfEnvs = ['dev', 'staging', 'rc', 'production', 'test', 'qa']
-   def NEXUS_REPO_URL = "http://repository.nextiva.xyz/repository/static-assets-" + deployEnvironment
+   def nexusRepoUrl = "http://repository.nextiva.xyz/repository/static-assets-" + deployEnvironment
 
    if (deployEnvironment in listOfEnvs) {
-	sh "echo ${env.WORKSPACE}"
- 	sh "echo ${NEXUS_REPO_URL}"
-	sh "echo ${assetPath}"
-	sh "echo ${jobName}"
+        if (assetDir != null) {
+        sh '''
+           cd ${assetDir}
+           tar -czvf ${assetPath} ./
+           curl --show-error --fail --write-out "\nStatus: %{http_code}\n" -v -K /etc/nexus_curl_config --upload-file ${assetPath} ${nexusRepoUrl}/${packageName}
+           curl --show-error --fail --write-out "\nStatus: %{http_code}\n" -v -K /etc/nexus_curl_config --upload-file ${assetPath} ${nexusRepoUrl}/${packageName}-${version}
+        '''
+        }
+
    }
    else {
-   	return 1
+        return 1
    }
 }
