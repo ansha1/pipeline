4ed528c2b44691a314150c817fbb0d71b45694a6
Ivan Fishchuk <ivanfishchuk@Ivans-MacBook-Pro.local>
2018-03-13 18:52:05 +0200

    updated shared library

--- vars/archiveToNexus.groovy
+++ vars/archiveToNexus.groovy
@@ -1,16 +1,15 @@
-def call(def deployEnvironment/*, def assetDir, def version, def packageName*/) {
+def call(def deployEnvironment, def assetDir, def version, def packageName) {
   
-   def assetPath = "${env.EXECUTOR_NUMBER}" + '.bzip'
    def jobName = "${env.JOB_NAME}"
    def listOfEnvs = ['dev', 'staging', 'rc', 'production', 'test', 'qa']
    def nexusRepoUrl = "http://repository.nextiva.xyz/repository/static-assets-" + deployEnvironment
+   def assetPath = "${env.WORKSPACE}" + "/" + packageName + "-" + "${env.EXECUTOR_NUMBER}" + ".bzip"
 
    if (deployEnvironment in listOfEnvs) {
         if (assetDir != null) {
         generateBuildProperties()
         sh '''
-           cd ${assetDir}
-           tar -czvf ${assetPath} ./
+           cd ${assetDir} && tar -czvf ${assetPath} ./
            curl --show-error --fail --write-out "\nStatus: %{http_code}\n" -v -K /etc/nexus_curl_config --upload-file ${assetPath} ${nexusRepoUrl}/${packageName}
            curl --show-error --fail --write-out "\nStatus: %{http_code}\n" -v -K /etc/nexus_curl_config --upload-file ${assetPath} ${nexusRepoUrl}/${packageName}-${version}
         '''
