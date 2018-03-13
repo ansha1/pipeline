a5d222435e50f7af9e4ce5a0c487505e29781d06
Ivan Fishchuk <ivanfishchuk@Ivans-MacBook-Pro.local>
2018-03-13 13:43:00 +0200

    adding archiveToNexus.groovy

--- vars/archiveToNexus.groovy
+++ vars/archiveToNexus.groovy
@@ -1,6 +1,6 @@
 def call(/*def deployEnvironment, def assetDir, def version, def upstreamJobName, def packageName*/) {
 
-   def assetPath = '${env.EXECUTOR_NUMBER}.bzip'
+   def assetPath = ${env.EXECUTOR_NUMBER} + '.bzip'
    def jobName = /*upstreamJobName ?:*/ ${env.JOB_NAME}
    def listOfEnvs = ['dev', 'staging', 'rc', 'production', 'test', 'qa']
    //def NEXUS_REPO_URL = "http://repository.nextiva.xyz/repository/static-assets-" + deployEnvironment
