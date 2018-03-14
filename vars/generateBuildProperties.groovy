8dd692061e27e841c2fa3e63151cede084aa9c51
Ivan Fishchuk <ivanfishchuk@Ivans-MacBook-Pro.local>
2018-03-14 16:01:42 +0200

    simple test

--- vars/generateBuildProperties.groovy
+++ vars/generateBuildProperties.groovy
@@ -3,20 +3,10 @@ def call(){
     sh "echo ${buildFileName} && cd ${env.WORKSPACE}"
     sh "echo writing build info to ${buildFileName} to ${env.WORKSPACE}"
 
-targetFile = new File(buildFileName);
-println "attempting to create file: $targetFile"
-println "the result: " + targetFile.createNewFile()
+    writeFile file: 'build.properties', text: '/* build.properties */'
 
-if (targetFile.createNewFile()) {
-    println "Successfully created file $targetFile"
-} else {
-    println "Failed to create file $targetFile"
-}
-
-/*
     def file = new File(buildFileName)
     def w = file.newWriter()
     w << "Another way"
     w.close()
-*/
 }
