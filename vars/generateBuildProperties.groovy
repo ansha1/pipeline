92b3b0037849331db21405b016b1942d7311086d
Ivan Fishchuk <ivanfishchuk@Ivans-MacBook-Pro.local>
2018-03-14 16:30:43 +0200

    simple test

--- vars/generateBuildProperties.groovy
+++ vars/generateBuildProperties.groovy
@@ -2,10 +2,19 @@ def call() {
     def buildFileName = "build.properties"
     sh "echo ${buildFileName} && cd ${env.WORKSPACE}"
     sh "echo writing build info to ${buildFileName} to ${env.WORKSPACE}"
-
+/*
     writeFile file: 'build.properties', text: '/* build.properties */'
-
     def w = file.newWriter()
     w << "Another way"
     w.close()
+*/
+
+if(build.workspace.isRemote()){
+channel = build.workspace.channel
+println "we definately are running on slave"
+}
+String fp = build.workspace.toString() + "/" + "newfile.txt"
+newFile = new hudson.FilePath(channel, fp)
+newFile.write("xyz", null)
+
 }
