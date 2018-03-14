148fda7b03c775fffe4c6908024dc4bfb9a05835
Ivan Fishchuk <ivanfishchuk@Ivans-MacBook-Pro.local>
2018-03-14 18:35:20 +0200

    simple test

--- vars/generateBuildInfo.groovy
+++ vars/generateBuildInfo.groovy
@@ -10,5 +10,14 @@ def call(){
     '''
 */
 
-return "Just for test" 
+    def toReturn = sh returnStdout: true, script: """
+    echo "build_version=${VERSION}"
+    echo "commit=$(git rev-parse HEAD)"
+    echo "deploy_environment=${DEPLOY_ENVIRONMENT}"
+    echo "job_name=${JOB_NAME}"
+    echo "build_date_time=$(date)"
+    echo "repository_url=$(git config --get remote.origin.url)"
+    """
+
+return toReturn 
 }
