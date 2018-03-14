12ab034313ae02b78db1f4b1fa347e2225d1d5a2
Ivan Fishchuk <ivanfishchuk@Ivans-MacBook-Pro.local>
2018-03-14 18:51:54 +0200

    simple test

--- vars/generateBuildProperties.groovy
+++ vars/generateBuildProperties.groovy
@@ -1,4 +1,4 @@
-def call() {
+def call(def deployEnvironment, def version, def jobName) {
     def buildFileName = 'build.properties'
     def buildPropertiesVar = "/* build properties /*/n"
     buildPropertiesVar += generateBuildInfo(deployEnvironment, version, jobName)
