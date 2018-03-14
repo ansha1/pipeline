d887d37ebb8cfef84feb632dfaca5da7a909e1e9
Ivan Fishchuk <ivanfishchuk@Ivans-MacBook-Pro.local>
2018-03-14 16:36:45 +0200

    simple test

--- vars/generateBuildProperties.groovy
+++ vars/generateBuildProperties.groovy
@@ -2,13 +2,13 @@ def call() {
     def buildFileName = "build.properties"
     sh "echo ${buildFileName} && cd ${env.WORKSPACE}"
     sh "echo writing build info to ${buildFileName} to ${env.WORKSPACE}"
-/*
     writeFile file: 'build.properties', text: '/* build.properties */'
+    buildFileName << generateBuildInfo()
+/*
     def w = file.newWriter()
     w << "Another way"
     w.close()
 */
 
-println Jenkins.getInstance().getComputer(env['NODE_NAME']).getChannel()
 
 }
