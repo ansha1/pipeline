45a98a312c77153d12b9f9a4de4f180940b2e6df
Ivan Fishchuk <ivanfishchuk@Ivans-MacBook-Pro.local>
2018-03-14 14:09:02 +0200

    move build.properties generation to archiveToNexus.groovy

--- vars/archiveToNexus.groovy
+++ vars/archiveToNexus.groovy
@@ -11,12 +11,12 @@ def call(def deployEnvironment, def assetDir, def version, def packageName) {
 
 
 	sh """
-	    echo \"build_version=${version}\" > build.properties
-	    echo \"commit=$(git rev-parse HEAD)\" >> build.properties
-	    echo \"deploy_environment=${deployEnvironment}\" >> build.properties
-	    echo \"job_name=${jobName}\" >> build.properties
-	    echo \"build_date_time=$(date)\" >> build.properties
-	    echo \"repository_url=$(git config --get remote.origin.url)\" >> build.properties
+	    echo 'build_version=${version}' > build.properties
+	    echo 'commit=$(git rev-parse HEAD)' >> build.properties
+	    echo 'deploy_environment=${deployEnvironment}' >> build.properties
+	    echo 'job_name=${jobName}' >> build.properties
+	    echo 'build_date_time=$(date)' >> build.properties
+	    echo 'repository_url=$(git config --get remote.origin.url)' >> build.properties
     	"""
 /*
         sh """
