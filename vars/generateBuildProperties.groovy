ec6a18ec2acbbb4cc41dfc5ed850892247a173d9
Ivan Fishchuk <ivanfishchuk@Ivans-MacBook-Pro.local>
2018-03-14 13:07:02 +0200

    redirect Build information to build.properties

--- vars/generateBuildProperties.groovy
+++ vars/generateBuildProperties.groovy
@@ -1,6 +1,6 @@
 def call(){
     def buildFileName = "${env.WORKSPACE}" + "/" + "build.properties"
-    def createBuildFileName = new File(buildFileName)
+    def createBuildFileName = new File(buildFileName).createNewFile()
     def writeToFile = createBuildFileName.newWriter()
     
     sh "echo writing build info to ${BUILD_FILENAME} to ${env.WORKSPACE}"
