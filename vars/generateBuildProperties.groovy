1853cb0b1b2e218315ed4cdfbde33e3ac7d7af6c
Ivan Fishchuk <ivanfishchuk@Ivans-MacBook-Pro.local>
2018-03-14 18:01:22 +0200

    simple test

--- vars/generateBuildProperties.groovy
+++ vars/generateBuildProperties.groovy
@@ -1,26 +1,10 @@
-import jenkins.*
-import jenkins.model.*
-import hudson.*
-import hudson.model.*
-import hudson.util.*
-
 def call() {
     def buildFileName = "build.properties"
     def path = "${env.WORKSPACE}" + "/" + buildFileName
     //sh "echo ${buildFileName} && cd ${env.WORKSPACE}"
     //sh "echo writing build info to ${buildFileName} to ${env.WORKSPACE}"
-    //writeFile file: 'build.properties', text: '/* build.properties */
+    writeFile file: buildFileName, text: path
 
-manager.listener.logger.println manager.build.project.getWorkspace()
-manager.listener.logger.println manager.build.workspace
-
-if (manager.build.workspace.isRemote()){
-    channel = manager.build.workspace.channel
-    manager.listener.logger.println  "I AM REMOTE!!"
-}
-
-fp = manager.build.workspace.toString() + "/repo_name/" + "mydeps.file"
-newFile = new hudson.FilePath(channel, fp)
 
 
 //    buildFileName << generateBuildInfo()
