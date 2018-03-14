5f6abb7dab44ed04ffb96385dd21232a58723c3b
Ivan Fishchuk <ivanfishchuk@Ivans-MacBook-Pro.local>
2018-03-14 19:01:11 +0200

    simple test

--- vars/generateBuildInfo.groovy
+++ vars/generateBuildInfo.groovy
@@ -4,13 +4,17 @@ def call(def deployEnvironment, def version, def jobName) {
 
     def toReturn = sh returnStdout: true, script: '''
     echo ""
-    echo "build_version=${version}"
     echo "commit=$(git rev-parse HEAD)"
-    echo "deploy_environment=${deployEnvironment}"
-    echo "job_name=${jobName}"
     echo "build_date_time=$(date)"
     echo "repository_url=$(git config --get remote.origin.url)"
     '''
+
+    def toReturn += sh returnStdout: true, script: """
+	echo ""
+	echo "build_version=${version}"
+	echo "deploy_environment=${deployEnvironment}"
+	echo "job_name=${jobName}"
+    """
 
     return toReturn 
 }
