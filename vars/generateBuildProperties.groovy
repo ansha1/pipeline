963a59117fa3802aed2d1accac0f0b0ffa657ab6
Ivan Fishchuk <ivanfishchuk@Ivans-MacBook-Pro.local>
2018-03-14 18:23:26 +0200

    simple test

--- vars/generateBuildProperties.groovy
+++ vars/generateBuildProperties.groovy
@@ -1,7 +1,7 @@
 def call() {
     def buildFileName = 'build.properties'
     def buildPropertiesVar = '/* build properties /*'
-    buildPropertiesVar << generateBuildInfo()
+    buildPropertiesVar += "/n" + generateBuildInfo()
     println buildPropertiesVar
     writeFile file: buildFileName, text: buildPropertiesVar
 }
