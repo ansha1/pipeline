6f97014f3e7b647c1ba8afc8949c18cc28833e91
Ivan Fishchuk <ivanfishchuk@Ivans-MacBook-Pro.local>
2018-03-14 18:08:28 +0200

    simple test

--- vars/generateBuildProperties.groovy
+++ vars/generateBuildProperties.groovy
@@ -1,18 +1,7 @@
 def call() {
-    def buildFileName = "build.properties"
-    def path = "${env.WORKSPACE}" + "/" + buildFileName
-    //sh "echo ${buildFileName} && cd ${env.WORKSPACE}"
-    //sh "echo writing build info to ${buildFileName} to ${env.WORKSPACE}"
-    writeFile file: buildFileName, text: path
-
-
-
-//    buildFileName << generateBuildInfo()
-/*
-    def w = file.newWriter()
-    w << "Another way"
-    w.close()
-*/
-
-
+    def buildFileName = 'build.properties'
+    def buildPropertiesVar = '/* build properties /*'
+    buildPropertiesVar << generateBuildInfo()
+    println buildPropertiesVar
+    //writeFile file: buildFileName, text: path
 }
