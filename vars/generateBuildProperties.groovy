13dad0cc12f2c7fe33495c565b739b933bc3cd37
Ivan Fishchuk <ivanfishchuk@Ivans-MacBook-Pro.local>
2018-03-14 17:00:23 +0200

    simple test

--- vars/generateBuildProperties.groovy
+++ vars/generateBuildProperties.groovy
@@ -4,14 +4,23 @@ import jenkins.model.Jenkins;
 def call() {
     def buildFileName = "build.properties"
     def path = "${env.WORKSPACE}" + "/" + buildFileName
-    sh "echo ${buildFileName} && cd ${env.WORKSPACE}"
+    //sh "echo ${buildFileName} && cd ${env.WORKSPACE}"
     //sh "echo writing build info to ${buildFileName} to ${env.WORKSPACE}"
     //writeFile file: 'build.properties', text: '/* build.properties */'
 
-    println Jenkins.getInstance().getComputer(env['NODE_NAME'])
-    def createBuildFileName = new FilePath(Jenkins.getInstance().getComputer(env['NODE_NAME']).getChannel(), path);
-    createBuildFileName.newWriter().withWriter { w ->
-      w << "HEHE
+if(build.workspace.isRemote())
+{
+    channel = build.workspace.channel;
+    fp = new FilePath(channel, build.workspace.toString() + "/node_details.txt")
+} else {
+    fp = new FilePath(new File(build.workspace.toString() + "/node_details.txt"))
+}
+
+if(fp != null)
+{
+    fp.write("test data", null); //writing to file
+} 
+
     }
 
 
