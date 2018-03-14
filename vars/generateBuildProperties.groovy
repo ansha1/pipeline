ba36f767b0c8ddf5b2bedae26b055a0f401df0ee
Ivan Fishchuk <ivanfishchuk@Ivans-MacBook-Pro.local>
2018-03-14 16:45:36 +0200

    simple test

--- vars/generateBuildProperties.groovy
+++ vars/generateBuildProperties.groovy
@@ -1,9 +1,21 @@
+import hudson.FilePath;
+import jenkins.model.Jenkins;
+
 def call() {
     def buildFileName = "build.properties"
+    def path = "${env.WORKSPACE}" + "/" + buildFileName
     sh "echo ${buildFileName} && cd ${env.WORKSPACE}"
-    sh "echo writing build info to ${buildFileName} to ${env.WORKSPACE}"
-    writeFile file: 'build.properties', text: '/* build.properties */'
-    buildFileName << generateBuildInfo()
+    //sh "echo writing build info to ${buildFileName} to ${env.WORKSPACE}"
+    //writeFile file: 'build.properties', text: '/* build.properties */'
+
+    println Jenkins.getInstance().getComputer(env['NODE_NAME'])
+    def createBuildFileName = new FilePath(Jenkins.getInstance().getComputer(env['NODE_NAME']).getChannel(), path);
+    createBuildFileName.newWriter().withWriter { w ->
+      w << "HEHE
+    }
+
+
+//    buildFileName << generateBuildInfo()
 /*
     def w = file.newWriter()
     w << "Another way"
