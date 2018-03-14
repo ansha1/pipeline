319d443b76e2aabfd50090372b2c6527dc57c7f1
Ivan Fishchuk <ivanfishchuk@Ivans-MacBook-Pro.local>
2018-03-14 12:42:02 +0200

    redirect Build information to build.properties

--- vars/generateBuildProperties.groovy
+++ vars/generateBuildProperties.groovy
@@ -1,5 +1,6 @@
 def call(){
     BUILD_FILENAME="build.properties"
-    sh "echo writing build info to ${BUILD_FILENAME}"
+
+    sh "echo writing build info to ${BUILD_FILENAME} to ${env.WORKSPACE}"
     new File("${env.WORKSPACE}", BUILD_FILENAME) << generateBuildInfo()
 }
