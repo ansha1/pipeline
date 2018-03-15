35f57d7ea4d0c891ceb41512dbc789d69ed0c840
Ivan Fishchuk <ivanfishchuk@Ivans-MacBook-Pro.local>
2018-03-15 10:31:37 +0200

    simple test

--- vars/archiveToNexus.groovy
+++ vars/archiveToNexus.groovy
@@ -9,13 +9,12 @@ def call(def deployEnvironment, def assetDir, def version, def packageName) {
         if (assetDir != null) {
         generateBuildProperties(deployEnvironment, version, jobName)
 
-/*
         sh """
            cd ${assetDir} && tar -czvf ${assetPath} ./
            curl --show-error --fail --write-out "\nStatus: %{http_code}\n" -v -K /etc/nexus_curl_config --upload-file ${assetPath} ${nexusRepoUrl}/${packageName}
            curl --show-error --fail --write-out "\nStatus: %{http_code}\n" -v -K /etc/nexus_curl_config --upload-file ${assetPath} ${nexusRepoUrl}/${packageName}-${version}
         """
-*/
+
         }
    }
    else {
