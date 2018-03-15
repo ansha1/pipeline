1b426685093f92ef00d3558d15560ed6b48b5fe1
Ivan  Fishchuk <ivan.fishchuk@nextiva.com>
2018-03-15 03:15:00 -0700

    archiveToNexus.groovy edited online with Bitbucket

--- vars/archiveToNexus.groovy
+++ vars/archiveToNexus.groovy
@@ -7,16 +7,13 @@ def call(def deployEnvironment, def assetDir, def version, def packageName) {
    def assetPath = "${env.WORKSPACE}" + "/" + packageName + "-" + "${env.EXECUTOR_NUMBER}" + ".bzip"
 
    if (deployEnvironment in listOfEnvs) {
-        if (assetDir != null) {
-        generateBuildProperties(deployEnvironment, version, jobName)
-
+     
+     	generateBuildProperties(deployEnvironment, version, jobName)
         sh """
            cd ${assetDir} && cp ${env.WORKSPACE}/${buildFileName} ./ && tar -czvf ${assetPath} ./
            curl --show-error --fail --write-out "\nStatus: %{http_code}\n" -v -K /etc/nexus_curl_config --upload-file ${assetPath} ${nexusRepoUrl}/${packageName}
            curl --show-error --fail --write-out "\nStatus: %{http_code}\n" -v -K /etc/nexus_curl_config --upload-file ${assetPath} ${nexusRepoUrl}/${packageName}-${version}
         """
-
-        }
    }
    else {
        throw new IllegalArgumentException("Provided env ${deployEnvironment} is not in the list ${listOfEnvs}")
