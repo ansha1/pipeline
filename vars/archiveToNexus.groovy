f5b2c214bb7a94fb0f30e5e8719ef1073f6f9a15
Ivan  Fishchuk <ivan.fishchuk@nextiva.com>
2018-03-16 04:59:34 -0700

    Merge pull request #9 in REL/pipelines from archive_to_nexus to develop
    Squashed commit of the following:
    
    commit bdca8e4f1f1e86ae1bd393876643bb8a9a0a3c6e
    Author: Ivan Fishchuk <ivanfishchuk@Ivans-MacBook-Pro.local>
    Date:   Fri Mar 16 13:52:35 2018 +0200
    
        load static vars of class SharedJobsStaticVars
    
    commit 927d2bbbce2a3e585dc1f43ee6d52040f22d48ea
    Author: Ivan Fishchuk <ivanfishchuk@Ivans-MacBook-Pro.local>
    Date:   Fri Mar 16 13:48:18 2018 +0200
    
        load static vars of class SharedJobsStaticVars
    
    commit 6994a4d0da5cf22731c03af6457c28a1f9462ab2
    Author: Ivan Fishchuk <ivanfishchuk@Ivans-MacBook-Pro.local>
    Date:   Fri Mar 16 13:43:40 2018 +0200
    
        load static vars of class SharedJobsStaticVars
    
    commit e370c554521e418b1f25465b320ab0814e28bc42
    Author: Ivan Fishchuk <ivanfishchuk@Ivans-MacBook-Pro.local>
    Date:   Fri Mar 16 13:37:48 2018 +0200
    
        load static vars of class SharedJobsStaticVars

--- vars/archiveToNexus.groovy
+++ vars/archiveToNexus.groovy
@@ -1,23 +1,21 @@
-import com.nextiva.SharedJobsStaticVars
+import static com.nextiva.SharedJobsStaticVars.*
 
 def call(def deployEnvironment, def assetDir, def version, def packageName) {
 
-   SharedJobsStaticVars globalVars = new SharedJobsStaticVars()
+  def jobName = "${env.JOB_NAME}"
+  def nexusRepoUrl = NEXUS_STATIC_ASSETS_REPO_URL + deployEnvironment
+  def assetPath = "${env.WORKSPACE}" + "/" + packageName + "-" + "${env.EXECUTOR_NUMBER}" + ASSETS_PACKAGE_EXTENSION
 
-   def jobName = "${env.JOB_NAME}"
-   def nexusRepoUrl = globalVars.NEXUS_STATIC_ASSETS_REPO_URL + deployEnvironment
-   def assetPath = "${env.WORKSPACE}" + "/" + packageName + "-" + "${env.EXECUTOR_NUMBER}" + globalVars.ASSETS_PACKAGE_EXTENSION
+  if (deployEnvironment in LIST_OF_ENVS) {
 
-   if (deployEnvironment in globalVars.LIST_OF_ENVS) {
-     
-     	generateBuildProperties(deployEnvironment, version, jobName)
-        sh """
-           cd ${assetDir} && cp ${env.WORKSPACE}/${globalVars.BUILD_FILENAME} ./ && tar -czvf ${assetPath} ./
-           curl --show-error --fail --write-out "\nStatus: %{http_code}\n" -v -K /etc/nexus_curl_config --upload-file ${assetPath} ${nexusRepoUrl}/${packageName}
-           curl --show-error --fail --write-out "\nStatus: %{http_code}\n" -v -K /etc/nexus_curl_config --upload-file ${assetPath} ${nexusRepoUrl}/${packageName}-${version}
-        """
-   }
-   else {
-       throw new IllegalArgumentException("Provided env ${deployEnvironment} is not in the list ${globalVars.listOfEnvs}")
-   }
+    generateBuildProperties(deployEnvironment, version, jobName)
+    sh """
+      cd ${assetDir} && cp ${env.WORKSPACE}/${BUILD_FILENAME} ./ && tar -czvf ${assetPath} ./
+      curl --show-error --fail --write-out "\nStatus: %{http_code}\n" -v -K /etc/nexus_curl_config --upload-file ${assetPath} ${nexusRepoUrl}/${packageName}
+      curl --show-error --fail --write-out "\nStatus: %{http_code}\n" -v -K /etc/nexus_curl_config --upload-file ${assetPath} ${nexusRepoUrl}/${packageName}-${version}
+    """
+  }
+  else {
+    throw new IllegalArgumentException("Provided env ${deployEnvironment} is not in the list ${LIST_OF_ENVS}")
+  }
 }
