c3ec7278a98a7b09cd66d72bd65ee69fe526f924
Evgeniy Sakhnyk <Evgeniy.Sakhnyk@nextiva.com>
2018-03-19 10:39:34 -0700

    Merge pull request #13 in REL/pipelines from develop to master
    Squashed commit of the following:
    
    commit 00e79dcfb5974d767598ba05477562b6fb37cdca
    Author: Evgeniy Sakhnyk <Evgeniy.Sakhnyk@nextiva.com>
    Date:   Mon Mar 19 10:37:59 2018 -0700
    
        Merge pull request #12 in REL/pipelines from feature/refactor-python-utils to develop
    
        Squashed commit of the following:
    
        commit 286a8ac71a626a66c5676b0c819acc858d15c232
        Author: Evgeniy Sakhnyk <Evgeniy.Sakhnyk@nextiva.com>
        Date:   Mon Mar 19 19:35:52 2018 +0200
    
            refactor pythonUtils
    
    commit 70a8930af6c9db20cca3869d01bb95aa647be34c
    Merge: 26c32a8 ca7c25c
    Author: Evgeniy Sakhnyk <Evgeniy.Sakhnyk@nextiva.com>
    Date:   Mon Mar 19 18:16:18 2018 +0200
    
        Merge tag 'v0.0.1' into develop
    
        initial version for shared libraries

--- vars/pythonUtils.groovy
+++ vars/pythonUtils.groovy
@@ -1,12 +1,12 @@
 import static com.nextiva.SharedJobsStaticVars.*
 
 
-def createVirtualEnv(String pythonName='python') {
+def createVirtualEnv(String pythonName='python', String venvDir=VENV_DIR) {
     println 'Create virtualenv.'
-    sh(script: "virtualenv --python=${pythonName} ${VENV_DIR}")
+    sh(script: "virtualenv --python=${pythonName} ${venvDir}")
 }
 
-def venvSh(String cmd, String venvDir=VENV_DIR, returnStdout=false) {
+def venvSh(String cmd, returnStdout=false, String venvDir=VENV_DIR) {
     println 'Activate virtualenv.'
     withEnv(getVirtualEnv()) {
         output = sh(returnStdout: returnStdout, script: cmd)
