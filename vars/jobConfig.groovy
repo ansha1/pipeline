a07964c1b0ea8f26b55afeb1c9fc1f1cecba95f9
Evgeniy Sakhnyk <Evgeniy.Sakhnyk@nextiva.com>
2018-05-02 03:40:10 -0700

    Merge pull request #38 in REL/pipelines from feature/add-ansible-env-customization to develop
    Squashed commit of the following:
    
    commit de6642be9279148520ffe04c77a016d9e11f62aa
    Author: Evgeniy Sakhnyk <Evgeniy.Sakhnyk@nextiva.com>
    Date:   Wed May 2 13:23:01 2018 +0300
    
        rename default envs
    
    commit 969cfeed009c06ef1b3b01545023c906cb75a029
    Author: Evgeniy Sakhnyk <Evgeniy.Sakhnyk@nextiva.com>
    Date:   Wed May 2 12:53:53 2018 +0300
    
        add ability to change ansible env with config

--- vars/jobConfig.groovy
+++ vars/jobConfig.groovy
@@ -5,17 +5,23 @@ def call(body) {
     body.delegate = pipelineParams
     body()
 
+    ansibleEnvMapDefault = [dev : "dev",
+                            qa: "qa",
+                            production : "production"]
+
     healthCheckMap = pipelineParams.healthCheckMap
     branchPermissionsMap = pipelineParams.branchPermissionsMap
+    ansibleEnvMap = pipelineParams.ansibleEnvMap.equals(null) ? ansibleEnvMapDefault : pipelineParams.ansibleEnvMap
     APP_NAME = pipelineParams.APP_NAME
     BASIC_INVENTORY_PATH = pipelineParams.BASIC_INVENTORY_PATH
     PLAYBOOK_PATH = pipelineParams.PLAYBOOK_PATH
     DEPLOY_APPROVERS = pipelineParams.DEPLOY_APPROVERS
     CHANNEL_TO_NOTIFY = pipelineParams.CHANNEL_TO_NOTIFY
 
+
     switch (env.BRANCH_NAME) {
         case 'dev':
-            ANSIBLE_ENV = 'dev'
+            ANSIBLE_ENV = ansibleEnvMap.get('dev')
             healthCheckUrl = healthCheckMap.get('dev')
             branchPermissions = branchPermissionsMap.get('dev')
             DEPLOY_ENVIRONMENT = 'dev'
@@ -23,21 +29,21 @@ def call(body) {
             echo('\nDEPRECATED: Please rename branch "dev" to "develop" to meet git-flow convention.\n')
             break
         case 'develop':
-            ANSIBLE_ENV = 'dev'
+            ANSIBLE_ENV = ansibleEnvMap.get('dev')
             healthCheckUrl = healthCheckMap.get('dev')
             branchPermissions = branchPermissionsMap.get('develop')
             DEPLOY_ENVIRONMENT = 'dev'
             DEPLOY_ON_K8S = true
             break
         case ~/^release\/.+$/:
-            ANSIBLE_ENV = 'qa'
+            ANSIBLE_ENV = ansibleEnvMap.get('qa')
             healthCheckUrl = healthCheckMap.get('qa')
             branchPermissions = branchPermissionsMap.get('qa')
             DEPLOY_ENVIRONMENT = 'production'
             DEPLOY_ON_K8S = false
             break
         case 'master':
-            ANSIBLE_ENV = 'production'
+            ANSIBLE_ENV = ansibleEnvMap.get('production')
             healthCheckUrl = healthCheckMap.get('production')
             branchPermissions = branchPermissionsMap.get('production')
             DEPLOY_ENVIRONMENT = 'production'
