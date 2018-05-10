ddf0e33fa602d1b0ea5c3ed4234eff0b8f1f39b1
Oleksandr Belonozhkin <oleksandr.belonozhkin@nextiva.com>
2018-05-10 10:50:46 -0700

    Merge pull request #43 in REL/pipelines from feature/hotfix-functions to develop
    Squashed commit of the following:
    
    commit eb6e9ba0be15e5cd195df8eee05101c0738acc0a
    Author: Oleksandr Belonozhkin <oleksandr.belonozhkin@nextiva.com>
    Date:   Thu May 10 20:26:48 2018 +0300
    
        "NOTIF-330: reminder for merging
    
    commit de0c16d4b0e42edf60fdd3f7e5d9aa2d8046f1ba
    Author: Oleksandr Belonozhkin <oleksandr.belonozhkin@nextiva.com>
    Date:   Thu May 10 20:09:55 2018 +0300
    
        Revert "NOTIF-330: fixed error that was causing conflicts when merging release into master after hotfix"
    
        This reverts commit 6833f12
    
    commit 6833f12588e96e6471733d976bebfd1487f7fae5
    Author: Oleksandr Belonozhkin <oleksandr.belonozhkin@nextiva.com>
    Date:   Thu May 10 19:06:32 2018 +0300
    
        NOTIF-330: fixed error that was causing conflicts when merging release into master after hotfix
    
    commit d52c56b8ebdbb6ba7068a96aa587ff5e55304676
    Author: Oleksandr Belonozhkin <oleksandr.belonozhkin@nextiva.com>
    Date:   Tue May 8 19:02:29 2018 +0300
    
        hotfix functions

---
+++ vars/hotfixFinish.groovy
@@ -0,0 +1,193 @@
+#!groovy
+import com.nextiva.*
+
+import static com.nextiva.SharedJobsStaticVars.*
+
+def call(body) {
+    def pipelineParams = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+
+    repositoryUrl = pipelineParams.repositoryUrl
+    developBranch = pipelineParams.developBranch
+    projectLanguage = pipelineParams.projectLanguage
+    autoPullRequest = pipelineParams.autoPullRequest
+    slackChannel = pipelineParams.slackChannel
+    versionPath = pipelineParams.versionPath.equals(null) ? '.' : pipelineParams.versionPath
+
+    //noinspection GroovyAssignabilityCheck
+    pipeline {
+
+        agent any
+
+        options {
+            timestamps()
+            skipStagesAfterUnstable()
+        }
+        tools {
+            jdk 'Java 8 Install automatically'
+            maven 'Maven 3.3.3 Install automatically'
+        }
+        stages {
+
+            stage('Checkout repo') {
+                steps {
+                    cleanWs()
+
+                    git branch: 'master', credentialsId: GIT_CHECKOUT_CREDENTIALS, url: repositoryUrl
+
+                }
+            }
+
+            stage('Prepare to finishing hotfix') {
+                steps {
+                    script {
+                        switch (projectLanguage) {
+                            case 'java':
+                                utils = new JavaUtils()
+                                break
+                            case 'python':
+                                utils = new PythonUtils()
+                                break
+                            case 'js':
+                                utils = new JsUtils()
+                                break
+                            default:
+                                error("""Incorrent programming language
+                                        please set one of the
+                                        supported languages:
+                                        java
+                                        python
+                                        js""")
+                                break
+                        }
+
+                        def hotfixBranches = sh(script: 'git branch -r', returnStdout: true)
+                                .tokenize('\n')
+                                .collect({ it.trim() })
+                                .findAll({ it ==~ /^origin\/hotfix\/\d+.\d+.\d+$/ })
+
+                        echo("Hotfix branch count: <<${hotfixBranches.size()}>>")
+                        switch (hotfixBranches.size()) {
+                            case 0:
+                                echo('There are no hotfix branches, please run HotfixStart Job first')
+                                currentBuild.rawBuild.result = Result.ABORTED
+                                throw new hudson.AbortException("\nThere no hotfix branches, please run HotfixStart Job first!!!\n")
+                                break
+                            case 1:
+                                hotfixBranch = hotfixBranches[0].replace('origin/', '')
+                                echo('Find hotfix branch (' + hotfixBranch + ')\ncontinue...\n')
+                                break
+                            default:
+                                echo('\n\nThere are more then 1 hotfix branch, please remove all but one and restart HotfixFinish Job!!!\n\n')
+                                currentBuild.rawBuild.result = Result.ABORTED
+                                throw new hudson.AbortException("\n\nThere are more then 1 hotfix branches, please leave one and restart HotfixFinish Job!!!\n\n")
+                                break
+                        }
+
+                        echo('Check branch naming for compliance with git-flow')
+                        if (hotfixBranch ==~ /^(hotfix\/\d+.\d+.\d+)$/) {
+                            echo('Parse hotfix version')
+                            sh """
+                                git fetch
+                                git checkout ${hotfixBranch}
+                            """
+                            hotfixVersion = utils.getVersion(versionPath)
+                            echo("\n\nFind hotfix version: ${hotfixVersion} \n\n")
+                        } else {
+                            error("""\n\nWrong hotfix branch name: ${hotfixBranch}
+                                    please use git-flow naming convention\n\n""")
+                        }
+
+                        if (developBranch ==~ /^(dev|develop)$/) {
+                            echo('Develop branch looks fine')
+                        } else {
+                            error("""\n\nWrong develop branch name : ${developBranch}
+                                    please use git-flow naming convention\n\n""")
+                        }
+                    }
+                }
+            }
+
+            stage('Merge hotfix branch to master') {
+                steps {
+                    script {
+                        try {
+                            sh """
+                                git fetch
+                                git checkout ${hotfixBranch}
+                                git checkout master
+                                git merge --no-ff ${hotfixBranch}
+                                git tag -a ${hotfixVersion} -m "Merge hotfix branch ${hotfixBranch} to master"
+                            """
+                        } catch (e) {
+                            currentBuild.rawBuild.result = Result.ABORTED
+                            throw new hudson.AbortException("\n\nCan`t merge ${hotfixBranch} to master \n You need to resolve merge conflicts manually and restart HotfixFinish Job\n\n")
+                        }
+                    }
+                }
+            }
+
+            stage('Merge hotfix branch to develop and releases') {
+                steps {
+                    script {
+                        def branchesOutput = sh script: 'git branch -r', returnStdout: true
+                        def branches = branchesOutput.tokenize('\n')
+                                .collect({ it.trim() })
+                                .findAll({ it ==~ /^origin\/release\/\d+.\d+.\d+$/ })
+                                .collect({ it.trim().replace("origin/", "") })
+                        branches.add(developBranch)
+                        echo("Branches to merge to: ${branches}")
+                        branches.each { branchToMerge ->
+                            mergeBranch(repositoryUrl, hotfixBranch, branchToMerge, slackChannel, autoPullRequest)
+                        }
+                    }
+                }
+            }
+
+            stage('Push changes in bitbucket') {
+                steps {
+                    sh """
+                        git push --all
+                        git push --tags
+                    """
+                }
+            }
+            stage('Delete hotfix branch') {
+                steps {
+                    script {
+                        sh """
+                            git push origin --delete ${hotfixBranch}
+                        """
+                    }
+                }
+            }
+        }
+    }
+}
+
+def mergeBranch(repositoryUrl, source, destination, channelToNotify, autoPullRequest) {
+    try {
+        sh """
+            git checkout ${destination}
+            git checkout ${source}
+            git merge --no-ff ${destination}
+        """
+        slackSend(color: '#00FF00', channel: channelToNotify, message: "merged ${source} into ${destination}")
+    } catch (e) {
+        print e
+        if (autoPullRequest) {
+            def newBranch = "${source}-to-${destination}"
+            sh """
+                git reset --hard
+                git checkout -b ${newBranch}
+                git push origin ${newBranch}
+            """
+            def prLink = createPr(repositoryUrl, newBranch, destination, "${source}->${destination}", 'DO NOT SELECT SQUASH OPTION WHEN MERGING THIS PR(if its enabled for the repository), otherwise there will be conflicts when merging release to master and releaseFinish job fill fail')
+            slackSend(color: '#6600cc',
+                    channel: channelToNotify,
+                    message: "Failed to automatically merge ${source} into ${destination}. Resolve conflicts and merge pull request (${prLink})")
+        }
+    }
+}
---
+++ vars/hotfixStart.groovy
@@ -0,0 +1,114 @@
+#!groovy
+import com.nextiva.*
+
+import static com.nextiva.SharedJobsStaticVars.*
+
+def call(body) {
+    def pipelineParams = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+
+    repositoryUrl = pipelineParams.repositoryUrl
+    projectLanguage = pipelineParams.projectLanguage
+    hotfixVersion = pipelineParams.hotfixVersion
+    versionPath = pipelineParams.versionPath.equals(null) ? '.' : pipelineParams.versionPath
+
+//noinspection GroovyAssignabilityCheck
+    pipeline {
+
+        agent any
+
+        options {
+            timestamps()
+            skipStagesAfterUnstable()
+        }
+        tools {
+            jdk 'Java 8 Install automatically'
+            maven 'Maven 3.3.3 Install automatically'
+        }
+        stages {
+            stage('Checkout repo') {
+                steps {
+                    cleanWs()
+
+                    git branch: 'master', credentialsId: GIT_CHECKOUT_CREDENTIALS, url: repositoryUrl
+
+                }
+            }
+            stage('Prepare for starting release') {
+                steps {
+                    script {
+
+                        switch (projectLanguage) {
+                            case 'java':
+                                utils = new JavaUtils()
+                                break
+                            case 'python':
+                                utils = new PythonUtils()
+                                break
+                            case 'js':
+                                utils = new JsUtils()
+                                break
+                            default:
+                                error("""Incorrent programming language
+                                        please set one of the
+                                        supported languages:
+                                        java
+                                        python
+                                        js""")
+                                break
+                        }
+                    }
+                }
+            }
+            stage('Collecting hotfix version') {
+                steps {
+                    script {
+                        echo "\nUserDefinedHotfixVersion: ${hotfixVersion}\n"
+                        hotfixVersion = hotfixVersion.equals('') ? getNextVersion(utils, versionPath) : hotfixVersion
+
+                        if (hotfixVersion ==~ /^(\d+.\d+.\d+)$/) {
+                            echo("\n\nSelected hotfix version: ${hotfixVersion}")
+                        } else {
+                            error("""\n\nWrong hotfix version : ${hotfixVersion}
+                                    please use git-flow naming convention\n\n""")
+                        }
+                    }
+                }
+            }
+            stage('Create hotfix branch') {
+                steps {
+                    script {
+                        utils.setVersion(hotfixVersion, versionPath)
+                        sh """
+                          git checkout -b hotfix/${hotfixVersion}
+                          git commit -a -m "Release engineering - bumped to ${hotfixVersion} patch version "
+                        """
+                    }
+                }
+            }
+            stage('Push to bitbucket repo') {
+                steps {
+                    script {
+                        sh """
+                          git push --all
+                        """
+                    }
+                }
+            }
+        }
+    }
+}
+
+String getNextVersion(utils, versionPath) {
+    def version = utils.getVersion(versionPath)
+    if (!(version ==~ /^(\d+.\d+.\d+)$/)) {
+        error("Wrong hotfix version: '${version}'")
+    }
+    def tokens = version.tokenize('.')
+    def major = tokens.get(0)
+    def minor = tokens.get(1)
+    def patch = tokens.get(2)
+    return major + "." + minor + "." + (patch.toInteger() + 1)
+}
