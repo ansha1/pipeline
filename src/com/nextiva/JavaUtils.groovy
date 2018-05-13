557fd226560633625f221509c1960c2287fa4aea
Evgeniy Sakhnyk <Evgeniy.Sakhnyk@nextiva.com>
2018-05-13 19:59:29 +0300

    inicial commit for job template

--- src/com/nextiva/JavaUtils.groovy
+++ src/com/nextiva/JavaUtils.groovy
@@ -1,5 +1,7 @@
 package com.nextiva
 
+import static com.nextiva.SharedJobsStaticVars.*
+
 String getVersion(String pathToPom = '.') {
     rootPom = readMavenPom file: "${pathToPom}/pom.xml"
     return rootPom.version
@@ -12,4 +14,61 @@ def setVersion(String version, String pathToPom = '.') {
 String createReleaseVersion(String version) {
     releaseVersion = version.replaceAll("-SNAPSHOT", "")
     return releaseVersion
+}
+
+def runSonarScanner(String projectVersion) {
+    scannerHome = tool SONAR_QUBE_SCANNER
+
+    withSonarQubeEnv(SONAR_QUBE_ENV) {
+        sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectVersion=${projectVersion}"
+    }
+}
+
+def test(String pathToSrc = '.') {
+    dir(pathToSrc) {
+        try {
+            sh 'mvn clean install jacoco:report'
+            sh 'mvn checkstyle:checkstyle'
+        } catch (e) {
+            error("Unit test fail ${e}")
+        } finally {
+            junit '**/target/surefire-reports/*.xml'
+            checkstyle canComputeNew: false, defaultEncoding: '', healthy: '', pattern: '**/target/checkstyle-result.xml', unHealthy: ''
+        }
+    }
+}
+
+def buildPublish(String pathToSrc = '.') {
+    dir(pathToSrc) {
+        try {
+            sh 'mvn deploy --batch-mode -DskipTests'
+        } catch (e) {
+            error("buildPublish  fail ${e}")
+
+        }
+    }
+}
+
+def setBuildVersion(String userDefinedBuildVersion, String pathToPom = '.') {
+
+    if (!userDefinedBuildVersion) {
+        version = utils.getVersion(pathToPom)
+        DEPLOY_ONLY = false
+    } else {
+        version = userDefinedBuildVersion.trim()
+        DEPLOY_ONLY = true
+        echo('===========================')
+        echo('User Defined Version = ' + version)
+    }
+
+    ANSIBLE_EXTRA_VARS = ['application_version': version,
+                          'maven_repo'         : version.contains('SNAPSHOT') ? 'snapshots' : 'releases']
+
+    BUILD_VERSION = version - "SNAPSHOT" + "-" + env.BUILD_ID
+    echo('===============================')
+    echo('POM VERSION ' + version)
+    echo('BUILD_VERSION ' + BUILD_VERSION)
+    echo('===============================')
+    print('DEPLOY_ONLY:  ' + DEPLOY_ONLY)
+    echo('===============================')
 }
---
+++ vars/jobTemplate.groovy
@@ -0,0 +1,170 @@
+#!groovy
+import static com.nextiva.SharedJobsStaticVars.*
+import com.nextiva.*
+
+def call(body) {
+    def pipelineParams = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+
+    healthCheckMap = pipelineParams.healthCheckMap
+    branchPermissionsMap = pipelineParams.branchPermissionsMap
+    ansibleEnvMap = pipelineParams.ansibleEnvMap.equals(null) ? ansibleEnvMapDefault : pipelineParams.ansibleEnvMap
+    APP_NAME = pipelineParams.APP_NAME
+    BASIC_INVENTORY_PATH = pipelineParams.BASIC_INVENTORY_PATH
+    PLAYBOOK_PATH = pipelineParams.PLAYBOOK_PATH
+    DEPLOY_APPROVERS = pipelineParams.DEPLOY_APPROVERS
+    CHANNEL_TO_NOTIFY = pipelineParams.CHANNEL_TO_NOTIFY
+
+
+    jobConfig {
+        healthCheckMap = [dev       : ["http://192.168.51.120:9020/LicenseService/health",
+                                       "http://192.168.51.121:9020/LicenseService/health"],
+                          qa        : ["http://10.103.50.50:9020/LicenseService/health",
+                                       "http://10.103.50.51:9020/LicenseService/health"],
+                          production: ["http://192.168.202.129:9020/LicenseService/health",
+                                       "http://192.168.202.130:9020/LicenseService/health",
+                                       "http://192.168.202.131:9020/LicenseService/health"]]
+        branchPermissionsMap = [dev       : ["authenticated"],
+                                qa        : ["rdavis", "avelichko", "avama", "skompanets", "sadgaonkar", "mkhunt"],
+                                production: ["rdavis", "avelichko", "avama", "skompanets", "sadgaonkar", "mkhunt"]]
+        APP_NAME = 'license-service'
+        BASIC_INVENTORY_PATH = 'ansible/role-based_playbooks/inventory/license/'
+        PLAYBOOK_PATH = 'ansible/role-based_playbooks/license-service.yml'
+        DEPLOY_APPROVERS = 'rdavis,ama,avelichko,sadgaonkar'
+        CHANNEL_TO_NOTIFY = 'iam_team_channel'
+    }
+    def securityPermissions = jobConfig.branchProperties
+    def DEPLOY_ON_SSO_SANDBOX = jobConfig.DEPLOY_ON_K8S
+
+//noinspection GroovyAssignabilityCheck
+    pipeline {
+
+        agent { label 'debian' }
+
+        tools {
+            jdk 'Java 8 Install automatically'
+            maven 'Maven 3.3.3 Install automatically'
+        }
+
+        options {
+            timestamps()
+            skipStagesAfterUnstable()
+            authorizationMatrix(inheritanceStrategy: nonInheriting(), permissions: securityPermissions)
+        }
+
+        parameters {
+            string(name: 'deploy_version', defaultValue: '', description: 'Set artifact version for skip all steps and deploy only \n' +
+                    'or leave empty for start full build')
+        }
+
+        stages {
+            stage('Set additional properties') {
+                steps {
+                    script {
+                        utils = jobConfig.utils
+                        utils.setBuildVersion(params.deploy_version)
+                        DEPLOY_ONLY = jobConfig.DEPLOY_ONLY
+                        DEPLOY_ON_K8S = jobConfig.DEPLOY_ON_K8S
+                    }
+                }
+            }
+
+            stage('Test') {
+                when {
+                    expression { DEPLOY_ONLY ==~ false && !(env.BRANCH_NAME ==~ /^(master)$/) }
+                }
+                parallel {
+                    stage('Unit tests') {
+                        steps {
+                            script {
+                                utils.test
+                            }
+                        }
+                    }
+                    stage('Sonar analyzing') {
+                        steps {
+                            script {
+                                utils.runSonarScanner(jobConfig.BUILD_VERSION)
+                            }
+                        }
+                    }
+                }
+            }
+            stage('Build') {
+                when {
+                    expression { DEPLOY_ONLY ==~ false && env.BRANCH_NAME ==~ /^(dev|hotfix\/.+|release\/.+)$/ }
+                }
+                parallel {
+                    stage('Publish build artifacts') {
+                        steps {
+                            script {
+                                utils.buildPublish()
+                            }
+                        }
+                    }
+                    stage('Publish docker image') {
+                        steps {
+                            buildPublishDockerImage(jobConfig.APP_NAME, jobConfig.BUILD_VERSION)
+                        }
+                    }
+                }
+            }
+            stage('Deploy') {
+                when {
+                    expression { env.BRANCH_NAME ==~ /^(dev|develop|master|release\/.+)$/ }
+                }
+                script {
+                    if (env.BRANCH_NAME ==~ /^(master|release\/.+)$/) {
+                        approve('Deploy on ' + jobConfig.ANSIBLE_ENV + '?', jobConfig.CHANNEL_TO_NOTIFY, jobConfig.DEPLOY_APPROVERS)
+
+                        isApproved = true //    = approve.isApproved()
+                    } else {
+                        //always approve dev deploy
+                        isApproved = true
+                    }
+                }
+                parallel {
+                    stage('Deploy in kubernetes') {
+                        when {
+                            expression { DEPLOY_ON_K8S ==~ true }
+                        }
+                        steps {
+                            script {
+                                echo("\n\nBUILD_VERSION: ${BUILD_VERSION}\n\n")
+                                kubernetes.deploy(jobConfig.APP_NAME, 'default', 'dev', BUILD_VERSION)
+                            }
+                        }
+                    }
+                    stage('Deploy on environment') {
+                        when {
+                            expression { isApproved ==~ true }
+                        }
+                        steps {
+                            script {
+                                runAnsiblePlaybook.releaseManagement(jobConfig.INVENTORY_PATH, jobConfig.PLAYBOOK_PATH, ANSIBLE_EXTRA_VARS)
+
+                                stage('Wait until service is up') {
+                                    try {
+                                        for (int i = 0; i < jobConfig.healthCheckUrl.size; i++) {
+                                            healthCheck(jobConfig.healthCheckUrl[i])
+                                        }
+                                    }
+                                    catch (e) {
+                                        error('Service startup failed ' + e)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        post {
+            always {
+                slackNotify(jobConfig.CHANNEL_TO_NOTIFY)
+            }
+        }
+    }
+}
