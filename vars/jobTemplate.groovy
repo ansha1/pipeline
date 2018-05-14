7e2ca839b39f1fe74804f19871b239fff6549440
Evgeniy Sakhnyk <Evgeniy.Sakhnyk@nextiva.com>
2018-05-14 16:35:19 +0300

    add language selection in job config

--- vars/jobTemplate.groovy
+++ vars/jobTemplate.groovy
@@ -129,7 +129,7 @@ def call(body) {
 //                        //always approve for dev branch
 //                        isApproved = true
 //                    }
-                }
+//                }
                 parallel {
                     stage('Deploy in kubernetes') {
                         when {
