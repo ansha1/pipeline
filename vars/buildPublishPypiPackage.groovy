2498b4c8575f8290a6aeb663d6bc605bcd4519f5
Maksym Vasylets <maksym.vasylets@nextiva.com>
2018-05-16 09:38:19 -0700

    Merge pull request #46 in REL/pipelines from ifishchuk/buildPublishPypiPackagegroovy-1526488648317 to develop
    Squashed commit of the following:
    
    commit 01e87e256c1fdf3a7cf092ba2590b0f90c0f6849
    Author: Ivan  Fishchuk <ivan.fishchuk@nextiva.com>
    Date:   Wed May 16 09:37:32 2018 -0700
    
        buildPublishPypiPackage.groovy edited online with Bitbucket

--- vars/buildPublishPypiPackage.groovy
+++ vars/buildPublishPypiPackage.groovy
@@ -4,8 +4,18 @@ import static com.nextiva.SharedJobsStaticVars.*
 def call(String extraPath='.', String deployEnvironment='test', String pythonName='python') {
     pythonUtils.createVirtualEnv(pythonName)
     pythonUtils.venvSh("""
-        pip install -U wheel
         cd ${extraPath}
+        pip install -U wheel
+
+		echo 'DEPRECATED: publishing to old repo pypi.nextiva.xyz'
+		pip install -U devpi-client
+		devpi use http://pypi.nextiva.xyz
+		devpi login root --password iampythonian
+		devpi use root/dev
+		python setup.py sdist bdist bdist_egg bdist_wheel
+		devpi upload --from-dir dist
+        
+        echo 'Publishing pypi to Nexus'
         python setup.py sdist bdist bdist_egg bdist_wheel
         twine upload --config-file /etc/nexus_pypi_config_${deployEnvironment} dist/*
     """)
