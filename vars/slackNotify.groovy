d7091ee5148539a4d0bf22de854617315330d145
Evgeniy Sakhnyk <Evgeniy.Sakhnyk@nextiva.com>
2018-05-17 18:42:32 +0300

    fix notify spelling

--- vars/slackNotify.groovy
+++ vars/slackNotify.groovy
@@ -3,7 +3,7 @@ def call(String notifyChannel) {
     notifyColor = ['SUCCESS': '#00FF00', 'FAILURE': '#FF0000', 'UNSTABLE': '#FF0000']
     commitinforaw = sh returnStdout: true, script: "git show --pretty=format:'The author was %an, %ar. Commit message: %s' | sed -n 1p"
     commitinfo = commitinforaw.trim()
-    def subject = "Build status: ${buildStatus} Job: ${env.JOB_NAME} #${env.BUILD_ID}"
+    def subject = "Build status: ${buildStatus} Job: ${env.JOB_NAME.replaceAll("%2F","_")} #${env.BUILD_ID}"
     def uploadSpec = """[
         {
             "title": "${subject}",
