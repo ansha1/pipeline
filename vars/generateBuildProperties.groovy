9590ab783c5df778a85e9e31a7384641bc2fd8e4
Adam Jenson <adam.jenson@nextiva.com>
2018-05-24 16:05:32 -0700

    Merge pull request #62 in REL/pipelines from bugfix/fix-jobname to develop
    Squashed commit of the following:
    
    commit 1015f2b1d94b9c09124d824459aab9314f578296
    Author: Erik Wilson <erik.wilson@nextiva.com>
    Date:   Thu May 24 16:04:25 2018 -0700
    
        replace %2F string with /

--- vars/generateBuildProperties.groovy
+++ vars/generateBuildProperties.groovy
@@ -7,7 +7,7 @@ def call(String deployEnvironment, String version, String jobName) {
 
     buildProperties.deploy_environment = deployEnvironment
     buildProperties.build_version = version
-    buildProperties.job_name = jobName
+    buildProperties.job_name = jobName.replaceAll('%2F','/')
     buildProperties.commit = sh returnStdout: true, script: 'git rev-parse HEAD'
     buildProperties.build_date_time = sh returnStdout: true, script: 'date'
     buildProperties.repository_url = sh returnStdout: true, script: 'git config --get remote.origin.url'
