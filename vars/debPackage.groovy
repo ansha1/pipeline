0d316f06a49b9aa08503e156eb0642a274dd5aaf
Maksym Vasylets <maksym.vasylets@nextiva.com>
2018-05-24 06:04:53 -0700

    Merge pull request #56 in REL/pipelines from feature/fix_deb_package to develop
    Squashed commit of the following:
    
    commit bab1c974d85e95caae9f642a7ad8534602f70781
    Author: Maksym Vasylets <maksym.vasylets@nextiva.com>
    Date:   Thu May 24 16:02:12 2018 +0300
    
        fix for build deb package

--- vars/debPackage.groovy
+++ vars/debPackage.groovy
@@ -22,6 +22,9 @@ import static com.nextiva.SharedJobsStaticVars.*
 
 def build(String packageName, String version, String deployEnvironment, String extraPath = null, def dockerImage = null) {
 
+    def pathToDebianFolder = ''
+    def buildLocation = ''
+
     if ( extraPath ) {
         println "We are going to build within " + extraPath
         pathToDebianFolder = WORKSPACE + "/" + extraPath + "/" + "debian"
@@ -76,6 +79,8 @@ def build(String packageName, String version, String deployEnvironment, String e
 }
 
 def publish(String packageName, String deployEnvironment, String extraPath = null) {
+
+    def buildLocation = ''
 
     if ( extraPath ) {
         buildLocation = WORKSPACE + "/" + extraPath
