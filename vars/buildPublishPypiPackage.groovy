32f230167cbc6bcb5a6f7032ad4667b21ad49297
Maksym Vasylets <maksym.vasylets@nextiva.com>
2018-05-29 05:36:22 -0700

    Merge pull request #64 in REL/pipelines from feature/improve_sdist_to_pypi to develop
    Squashed commit of the following:
    
    commit c3bd876560e3027072cc5e5b029b177e106d6910
    Author: Maksym Vasylets <maksym.vasylets@nextiva.com>
    Date:   Tue May 29 15:35:43 2018 +0300
    
        python3 default version
    
    commit ac5d17d143dc1717c7b5ef872e8ab3d80b10bc62
    Author: Maksym Vasylets <maksym.vasylets@nextiva.com>
    Date:   Tue May 29 12:30:05 2018 +0300
    
        refactoring

--- vars/buildPublishPypiPackage.groovy
+++ vars/buildPublishPypiPackage.groovy
@@ -1,22 +1,35 @@
 import static com.nextiva.SharedJobsStaticVars.*
 
 
-def call(String extraPath='.', String deployEnvironment='test', String pythonName='python') {
-    pythonUtils.createVirtualEnv(pythonName)
-    pythonUtils.venvSh("""
-        cd ${extraPath}
-        pip install -U wheel
+def call(String extraPath='.', String deployEnvironment=null, String pythonName='python3') {
+    dir(extraPath) {
+        pythonUtils.createVirtualEnv(pythonName)
+        pythonUtils.venvSh("""
+            pip install -U wheel
+            python setup.py sdist bdist bdist_egg bdist_wheel
+        """)
 
-        echo 'DEPRECATED: publishing to old repo pypi.nextiva.xyz'
-        pip install -U devpi-client
-        devpi use http://pypi.nextiva.xyz
-        devpi login root --password iampythonian
-        devpi use root/dev
-        python setup.py sdist bdist bdist_egg bdist_wheel
-        devpi upload --from-dir dist
+        pythonUtils.venvSh("""
+            echo 'DEPRECATED: publishing pypi-package to old repo - pypi.nextiva.xyz'
+            pip install -U devpi-client
+            devpi use http://pypi.nextiva.xyz
+            devpi login root --password iampythonian
+            devpi use root/dev
+            devpi upload --from-dir dist
+        """)
 
-        echo 'Publishing pypi to Nexus'
-        python setup.py sdist bdist bdist_egg bdist_wheel
-        twine upload --config-file /etc/nexus_pypi_config_${deployEnvironment} dist/*
-    """)
+        if ( deployEnvironment ) {
+            pythonUtils.venvSh("""
+                echo 'Publishing pypi-package to Nexus'
+                twine upload --config-file /etc/nexus_pypi_config_${deployEnvironment} dist/*
+            """)
+        }
+        else {
+            pythonUtils.venvSh("""
+                echo 'Publishing pypi-package to Nexus'
+                twine upload --config-file /etc/nexus_pypi_config_dev dist/*
+                twine upload --config-file /etc/nexus_pypi_config_production dist/*
+            """)
+        }
+    }
 }
