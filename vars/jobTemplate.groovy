329c80381985553ac23824a16952f00d26ca47a6
Ivan Fishchuk <ivan.fishchuk@nextiva.com>
2018-06-12 21:03:48 +0300

    implementation for custom channel

--- vars/jobTemplate.groovy
+++ vars/jobTemplate.groovy
@@ -70,7 +70,7 @@ def call(body) {
                         env.INVENTORY_PATH = jobConfig.INVENTORY_PATH
                         env.PLAYBOOK_PATH = jobConfig.PLAYBOOK_PATH
                         env.DEPLOY_ON_K8S = jobConfig.DEPLOY_ON_K8S
-                        env.CHANNEL_TO_NOTIFY = jobConfig.CHANNEL_TO_NOTIFY
+                        env.CHANNEL_TO_NOTIFY = jobConfig.channelToNotify
                         env.DEPLOY_ENVIRONMENT = jobConfig.DEPLOY_ENVIRONMENT
                         env.VERSION = jobConfig.version
                         env.BUILD_VERSION = jobConfig.BUILD_VERSION
