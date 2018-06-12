0d4cf4bfeb227b1027ba0f28b1384911c83970f8
Ivan Fishchuk <ivan.fishchuk@nextiva.com>
2018-06-12 16:22:32 +0300

    added support for custom CHANNEL_TO_NOTIFY

--- vars/jobConfig.groovy
+++ vars/jobConfig.groovy
@@ -31,8 +31,9 @@ def call(body) {
     branchNotifyRules = pipelineParams.branchNotifyRules.equals(null) ? LIST_OF_DEFAULT_BRANCH_PATTERNS : pipelineParams.branchNotifyRules
     DEPLOY_ON_K8S = pipelineParams.DEPLOY_ON_K8S.equals(null) ? false : pipelineParams.DEPLOY_ON_K8S
     if (pipelineParams.CHANNEL_TO_NOTIFY_PER_BRANCH.equals(null)) {
-        CHANNEL_TO_NOTIFY = [:]                    
-        CHANNEL_TO_NOTIFY << [LIST_OF_DEFAULT_BRANCH_PATTERNS : pipelineParams.CHANNEL_TO_NOTIFY]
+        CHANNEL_TO_NOTIFY = [:]
+        println("LIST_OF_DEFAULT_BRANCH_PATTERNS: ", LIST_OF_DEFAULT_BRANCH_PATTERNS)                
+        CHANNEL_TO_NOTIFY << [ LIST_OF_DEFAULT_BRANCH_PATTERNS.asImmutable() : pipelineParams.CHANNEL_TO_NOTIFY]
     } else {
         CHANNEL_TO_NOTIFY = pipelineParams.CHANNEL_TO_NOTIFY_PER_BRANCH
     }
