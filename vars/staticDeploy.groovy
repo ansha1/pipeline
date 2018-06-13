c3433b5a495298b2b084020d1d88b346e094f7ab
Maksym Vasylets <maksym.vasylets@nextiva.com>
2018-06-13 12:00:06 +0300

    static deploy

---
+++ vars/staticDeploy.groovy
@@ -0,0 +1,10 @@
+import static com.nextiva.SharedJobsStaticVars.*
+
+
+static final  BASIC_INVENTORY_PATH = 'ansible/role-based_playbooks/inventory/static-deploy'
+
+def call(String componentName, String deployEnvironment, String deployVersion) {
+    def repoDir = prepareRepoDir(RELEASE_MANAGEMENT_REPO_URL, RELEASE_MANAGEMENT_REPO_BRANCH)
+    runAnsiblePlaybook(repoDir, "${BASIC_INVENTORY_PATH}/${deployEnvironment}",
+                       PLAYBOOK_PATH, ['version': deployVersion, 'component_name': componentName, 'static_assets_files': componentName])
+}
