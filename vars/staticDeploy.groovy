d9f1eea3129baca8f4a4a2dcc4db56290397d9cf
Maksym Vasylets <maksym.vasylets@nextiva.com>
2018-06-14 00:03:46 +0300

    test

--- vars/staticDeploy.groovy
+++ vars/staticDeploy.groovy
@@ -1,7 +1,7 @@
 import static com.nextiva.SharedJobsStaticVars.*
 
 
-def call(String componentName, String deployEnvironment, String deployVersion, String nextivaRepo) {
+def call(String componentName, String deployEnvironment, String deployVersion, String nextivaRepo=null) {
     def BASIC_INVENTORY_PATH = 'ansible/role-based_playbooks/inventory/static-deploy'
     def PLAYBOOK_PATH = 'ansible/role-based_playbooks/static-deploy.yml'
 
@@ -9,7 +9,7 @@ def call(String componentName, String deployEnvironment, String deployVersion, S
     echo nextivaRepo
     echo "2222222"
 
-    //def nextivaRepo = nextivaRepo.equals(null) ? deployEnvironment : nextivaRepo
+    def nextivaRepo = nextivaRepo.equals(null) ? deployEnvironment : nextivaRepo
 
     def repoDir = prepareRepoDir(RELEASE_MANAGEMENT_REPO_URL, RELEASE_MANAGEMENT_REPO_BRANCH)
     runAnsiblePlaybook(repoDir, "${BASIC_INVENTORY_PATH}/${env}", PLAYBOOK_PATH, ['version': deployVersion, 'component_name': componentName, 'static_assets_files': componentName, 'nextiva_repo': nextivaRepo])
