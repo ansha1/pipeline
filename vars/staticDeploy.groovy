3055431f670091052cd5cadbb3bf8ec69843cc91
Maksym Vasylets <maksym.vasylets@nextiva.com>
2018-06-13 12:53:07 +0300

    remove unneeded space

--- vars/staticDeploy.groovy
+++ vars/staticDeploy.groovy
@@ -1,7 +1,7 @@
 import static com.nextiva.SharedJobsStaticVars.*
 
 
-static final  BASIC_INVENTORY_PATH = 'ansible/role-based_playbooks/inventory/static-deploy'
+static final BASIC_INVENTORY_PATH = 'ansible/role-based_playbooks/inventory/static-deploy'
 
 def call(String componentName, String deployEnvironment, String deployVersion) {
     def repoDir = prepareRepoDir(RELEASE_MANAGEMENT_REPO_URL, RELEASE_MANAGEMENT_REPO_BRANCH)
