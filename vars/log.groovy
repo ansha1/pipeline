0159fecc8e420388123c4e91ed1b85a62944ffa6
Maksym Vasylets <maksym.vasylets@nextiva.com>
2018-06-16 22:57:42 +0300

    tst

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,10 +1,10 @@
-def END_CHAR = '\033[0m '
-def YELLOW_BOLD = '\033[1;33m'
-def BLUE_BOLD = '\033[1;34m'
-def RED_BOLD = '\033[1;31m'
+END_CHAR = '\033[0m '
+YELLOW_BOLD = '\033[1;33m'
+BLUE_BOLD = '\033[1;34m'
+RED_BOLD = '\033[1;31m'
 
 
-def printColor(String message, String color){
+def printColor(String message, String color) {
     ansiColor('xterm') {
         echo "${color}${msg}${END_CHAR}"
     }
