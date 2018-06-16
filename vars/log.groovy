652fd47eae74f8b2500b6fd83f77fe5841601c95
Maksym Vasylets <maksym.vasylets@nextiva.com>
2018-06-16 23:04:34 +0300

    tst

--- vars/log.groovy
+++ vars/log.groovy
@@ -4,14 +4,14 @@ BLUE_BOLD = '\033[1;34m'
 RED_BOLD = '\033[1;31m'
 
 
-def printColor(String message, String color) {
+def printAnsiColor(String message, String color) {
     ansiColor('xterm') {
         echo "${color}${msg}${END_CHAR}"
     }
 }
 
 def info(message) {
-    printColor(message, BLUE_BOLD)
+    printAnsiColor(BLUE_BOLD)
 }
 
 def warning(message) {
