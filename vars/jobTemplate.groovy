9c2d339ec7efcd521702fe39315dcd5598ed5258
Oleksandr Kutsenko <Oleksandr.Kutsenko@nextiva.com>
2018-07-19 11:37:14 +0300

    set node per language

--- vars/jobTemplate.groovy
+++ vars/jobTemplate.groovy
@@ -41,7 +41,23 @@ def call(body) {
 //noinspection GroovyAssignabilityCheck
     pipeline {
 
-        agent { label jobConfig.nodeLabel }
+        //Set node label per language
+        if (jobConfig.nodeLabel != null) {
+            agent { label jobConfig.nodeLabel }
+        }
+
+        if (jobConfig.nodeLabel = null & jobConfig.projectFlow.language.equals('python')){
+           stage('Set agent node per python language'){
+           agent python
+        }
+        if (jobConfig.nodeLabel = null & jobConfig.projectFlow.language.equals('js')){
+           stage('Set agent node per js language'){
+           agent nodejs
+        }
+        if (jobConfig.nodeLabel = null & jobConfig.projectFlow.language.equals('java')){
+           stage('Set agent node per java language'){
+           agent java
+        }
 
         tools {
             jdk jobConfig.jdkVersion
