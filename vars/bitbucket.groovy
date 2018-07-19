3f1ccf7d89822a4cafc0f2c961b3f26959a562e4
Oleksandr Kutsenko <Oleksandr.Kutsenko@nextiva.com>
2018-07-19 17:50:38 +0300

    refactor

--- vars/bitbucket.groovy
+++ vars/bitbucket.groovy
@@ -4,10 +4,8 @@ import static com.nextiva.SharedJobsStaticVars.*
 String getSoruceBranchFromPr(String url) {
 
     def props = getPrFromUrl(url)
-
     def sourceBranch = props.fromRef.displayId.trim()
-    log("SourceBranch: ${sourceBranch}")
-
+    log.info("SourceBranch: ${sourceBranch}")
     return sourceBranch
 }
 
@@ -18,9 +16,9 @@ def prOwnerEmail(String url) {
 
 def getPrFromUrl(String url) {
 
-    log("Received PR url: ${url}")
+    log.info("Received PR url: ${url}")
     prUrl = url.replaceAll("git.nextiva.xyz/projects", "git.nextiva.xyz/rest/api/1.0/projects") - "/overview"
-    log("Transform Url for access via rest api: ${prUrl}")
+    log.info("Transform Url for access via rest api: ${prUrl}")
 
     def prResponce = httpRequest authentication: BITBUCKET_JENKINS_AUTH, httpMode: 'GET', url: prUrl
     def returnBody = readJSON text: prResponce.content
