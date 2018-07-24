df4e5a02dd897f6d3e111dcad368a002d1305b9b
Maksym Vasylets <maksym.vasylets@nextiva.com>
2018-07-24 23:51:11 +0300

    tst

--- vars/releaseStart.groovy
+++ vars/releaseStart.groovy
@@ -148,8 +148,8 @@ def call(body) {
             success {
                 script {
                     slack.notifyReleaseHotfix(CHANNEL_TO_NOTIFY, developmentVersion, 'Release', 'started')
-                    def user = common.getCurrentUser()
-                    /*def uploadSpec = """[{"title": "Release ${developmentVersion} started successfully!", "text": "Author: ${user}",
+                    /*def user = common.getCurrentUser()
+                    def uploadSpec = """[{"title": "Release ${developmentVersion} started successfully!", "text": "Author: ${user}",
                                         "color": "${SLACK_NOTIFY_COLORS.get(currentBuild.currentResult)}"]"""
                     call(CHANNEL_TO_NOTIFY, uploadSpec)*/
                 }
