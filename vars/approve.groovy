d89f05d4aae2ee8755969091b17b294ddc3ce267
Ivan Fishchuk <ivan.fishchuk@nextiva.com>
2018-07-26 16:01:18 +0300

    implement version auto increment

--- vars/approve.groovy
+++ vars/approve.groovy
@@ -7,6 +7,14 @@ def call(String message = 'Should we proceed', String slackChannel, String autho
     }
 }
 
+def sendToPrivate(String message = 'Should we proceed' , String UserSlackId, Integer minutes = 5) {
+    timeout(minutes) {
+        def buildMessage = buildApproveMessageBody(message)
+        slack.privateMessage(UserSlackId, buildMessage)
+        def inputResponse = input(id: 'Proceed', message: message, ok: 'Approve')
+    }
+}
+
 def buildApproveMessageBody(String message) {
     def uploadSpec = """[
         {
@@ -23,12 +31,6 @@ def buildApproveMessageBody(String message) {
             ]
         }
         ]"""
-}
 
-def sendToPrivate(String message = 'Should we proceed' , String UserSlackId, Integer minutes = 5 ) {
-    timeout(minutes) {
-        def buildMessage = buildApproveMessageBody(message)
-        slack.privateMessage(UserSlackId, buildMessage)
-        def inputResponse = input(id: 'Proceed', message: message, ok: 'Approve')
-    }
+    return uploadSpec
 }
