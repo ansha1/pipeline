2bd91c4c9a142f7fd3651938c8aa8a1696b70c23
Ivan Fishchuk <ivan.fishchuk@nextiva.com>
2018-07-26 15:22:50 +0300

    implement version auto increment

--- vars/approve.groovy
+++ vars/approve.groovy
@@ -16,8 +16,8 @@ def call(String message='Should we proceed', String slackChannel, String authori
         }
         ]"""
 
-        slack(slackChannel, uploadSpec)
+//        slack(slackChannel, uploadSpec)
+        slack.privateMessage(slackChannel, uploadSpec)
         def inputResponse = input(id: 'Proceed', message: message, ok: 'Approve', submitter: authorizedApprovers, submitterParameter: 'approver')
-        println('the anwer we get is: ' + inputResponse)
     }
 }
