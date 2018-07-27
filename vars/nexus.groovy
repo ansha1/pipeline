18762af5bedf710e548a86ad243ab9af41817c2d
Maksym Vasylets <maksym.vasylets@nextiva.com>
2018-07-27 15:21:31 +0300

    fix tests

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -90,7 +90,7 @@ def uploadFile(String filePath, String repoUrl, Boolean returnStatus = false) {
     withCredentials([
         file(credentialsId: 'nexus_curl_config', variable: 'NEXUS_CURL_CONFIG')
     ]) {
-        sh script: """curl ${verbose} --show-error --fail --write-out "\nStatus: %{http_code}\n" -K ${NEXUS_CURL_CONFIG} --upload-file ${filePath} ${repoUrl}""" name: 'curl', returnStatus: returnStatus  
+        sh script: """curl ${verbose} --show-error --fail --write-out "\nStatus: %{http_code}\n" -K ${NEXUS_CURL_CONFIG} --upload-file ${filePath} ${repoUrl}""", name: 'curl', returnStatus: returnStatus  
     }
 }
 
