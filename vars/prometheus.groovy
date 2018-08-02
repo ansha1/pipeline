e824a75e8908941010533a3a92f2aa0933e69508
Maksym Vasylets <maksym.vasylets@nextiva.com>
2018-08-02 17:35:02 +0300

    tst

--- vars/prometheus.groovy
+++ vars/prometheus.groovy
@@ -8,11 +8,19 @@ import io.prometheus.client.Summary
 import io.prometheus.client.exporter.PushGateway
 
 
-def getDurationTimer(){
-    def registry = new CollectorRegistry();
+def getRegistry(){
+    result new CollectorRegistry()
+}
+
+def getDurationTimer(def registry){
     def duration = Gauge.build()
         .name("my_batch_job_duration_seconds").help("Duration of my batch job in seconds.").register(registry);
     return duration.startTimer();
+}
+
+def pushGateway(def registry){
+    def pg = new PushGateway("10.103.50.110:9091");
+    pg.pushAdd(registry, "my_batch_job");
 }
 
 def event(){
@@ -27,7 +35,7 @@ def event(){
         .register(registry);
 
     try {
-        activeTransactions.inc(10)
+        activeTransactions.inc()
         // Your code here.
     
         // This is only added to the registry after success,
@@ -39,7 +47,7 @@ def event(){
         //sleep(20)
         echo('222')
 
-        activeTransactions.set(5)
+        activeTransactions.dec()
     } finally {
         durationTimer.setDuration();
         def pg = new PushGateway("10.103.50.110:9091");
