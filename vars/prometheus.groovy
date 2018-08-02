11395db7ec668bacab5d271319dfdb8f762f98f9
Maksym Vasylets <maksym.vasylets@nextiva.com>
2018-08-02 16:47:22 +0300

    tst

--- vars/prometheus.groovy
+++ vars/prometheus.groovy
@@ -8,18 +8,21 @@ import io.prometheus.client.Summary
 import io.prometheus.client.exporter.PushGateway
 
 
-def getDuration(){
-    log.warning('Duration1')
-  CollectorRegistry registry = new CollectorRegistry();
-  def duration = Gauge.build()
-     .name("my_batch_job_duration_seconds").help("Duration of my batch job in seconds.").register(registry);
-  //def durationTimer = duration.startTimer();
-  log.warning('Duration2')
-  return duration
-}
-
-
-def getPushGateway(){
-    def pg = new PushGateway("10.103.50.110:9091");
-    return pg
+def event(){
+    CollectorRegistry registry = new CollectorRegistry();
+    Gauge duration = Gauge.build()
+        .name("my_batch_job_duration_seconds").help("Duration of my batch job in seconds.").register(registry);
+    Gauge.Timer durationTimer = duration.startTimer();
+    try {
+        // Your code here.
+    
+        // This is only added to the registry after success,
+        // so that a previous success in the Pushgateway isn't overwritten on failure.
+        Gauge lastSuccess = Gauge.build()
+        .name("my_batch_job_last_success").help("Last time my batch job succeeded, in unixtime.").register(registry);
+        lastSuccess.setToCurrentTime();
+    } finally {
+        durationTimer.setDuration();
+        PushGateway pg = new PushGateway("10.103.50.110:9091");
+        pg.pushAdd(registry, "my_batch_job");
 }
