23694b01b1f6787afeca951b58edc9cde6d40372
Maksym Vasylets <maksym.vasylets@nextiva.com>
2018-08-02 16:21:53 +0300

    tst

--- vars/prometheus.groovy
+++ vars/prometheus.groovy
@@ -1,10 +1,11 @@
 @Grab('io.prometheus:simpleclient:0.4.0')
+@Grab('io.prometheus:simpleclient_pushgateway:0.4.0')
 import io.prometheus.client.CollectorRegistry
 import io.prometheus.client.Counter
 import io.prometheus.client.Gauge
 import io.prometheus.client.Histogram
 import io.prometheus.client.Summary
-import io.prometheus.client.PushGateway
+import io.prometheus.client.exporter.PushGateway
 
 
 def getRequestsCounter(){
