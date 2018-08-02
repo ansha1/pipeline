122c7cb852180d9019e75e39f02a966c399ebb14
Maksym Vasylets <maksym.vasylets@nextiva.com>
2018-08-02 16:32:33 +0300

    tst

--- vars/prometheus.groovy
+++ vars/prometheus.groovy
@@ -12,11 +12,11 @@ def getDurationTimer(){
   CollectorRegistry registry = new CollectorRegistry();
   Gauge duration = Gauge.build()
      .name("my_batch_job_duration_seconds").help("Duration of my batch job in seconds.").register(registry);
-  Gauge.Timer durationTimer = duration.startTimer();
+  def durationTimer = duration.startTimer();
   return durationTimer
 }
 
 def getPushGateway(){
-    PushGateway pg = new PushGateway("10.103.50.110:9091");
+    def pg = new PushGateway("10.103.50.110:9091");
     return pg
 }
