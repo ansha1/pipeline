96d9353b93730970afb97d33efdc00ff1f6b83d0
Maksym Vasylets <maksym.vasylets@nextiva.com>
2018-08-07 17:36:53 +0300

    send metrics for release start/finish hotfix start/finish

--- vars/hotfixFinish.groovy
+++ vars/hotfixFinish.groovy
@@ -174,6 +174,8 @@ def call(body) {
             }
             always {
                 script {
+                    prometheus.sendGauge(env.JOB_NAME, 'build_info', 1, prometheus.getBuildInfoMap(jobConfig))
+
                     if(currentBuild.currentResult != 'SUCCESS'){
                         slack.sendBuildStatusPrivatMessage(common.getCurrentUserSlackId())
                     }
