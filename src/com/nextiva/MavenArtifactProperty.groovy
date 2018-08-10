666087f40ec5063e4cc0b894a59b8cc73d8839c4
Ivan Fishchuk <ivan.fishchuk@nextiva.com>
2018-08-10 14:40:06 +0300

    implement Java artifact verification in Nexus

--- src/com/nextiva/MavenArtifactProperty.groovy
+++ src/com/nextiva/MavenArtifactProperty.groovy
@@ -1,14 +1,19 @@
 package com.nextiva
 
-import groovy.transform.TupleConstructor
-import groovy.transform.ToString
-
-
-@ToString
-@TupleConstructor()
+//import groovy.transform.TupleConstructor
+//import groovy.transform.ToString
+//
+//
+//@ToString
+//@TupleConstructor()
 class MavenArtifactProperty implements Serializable {
     String groupId
     String version
     String artifactId
     String packaging
+
+    @Override
+    String toString() {
+        return "class MavenArtifactProperty { groupId='$groupId' }"
+    }
 }
