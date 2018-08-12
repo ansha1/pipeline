1295b89970be1877ebc6090b399528ea979426e4
Maksym Vasylets <maksym.vasylets@nextiva.com>
2018-08-12 23:44:04 +0300

    branch approvers can approve increasing of patch version for release artifact

--- vars/approve.groovy
+++ vars/approve.groovy
@@ -7,11 +7,12 @@ def call(String message = 'Should we proceed', String slackChannel, String autho
     }
 }
 
-def sendToPrivate(String message = 'Should we proceed' , String UserSlackId, Integer minutes = 5) {
+def sendToPrivate(String message = 'Should we proceed' , String UserSlackId, List authorizedApprovers, Integer minutes = 5) {
     timeout(minutes) {
         def buildMessage = buildApproveMessageBody(message)
         slack.privateMessage(UserSlackId, buildMessage)
-        def inputResponse = input(id: 'Proceed', message: message, ok: 'Approve')
+        return input(id: 'Proceed', message: message, ok: 'Approve', submitter: authorizedApprovers.join(","),
+                submitterParameter: 'approver')
     }
 }
 
