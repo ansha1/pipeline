9a8e98cb645c02dd0458c9ca764128a1ec3f1458
Ivan Fishchuk <ivan.fishchuk@nextiva.com>
2018-08-14 12:55:49 +0300

    implement Java artifact verification in Nexus

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -41,7 +41,7 @@ Boolean checkNexus3Package(String repo, String format, String packageName, Strin
 Boolean checkNexus2Package(String repo, String format, String packageName, String packageVersion, String groupId) {
     def searchNexusQuery = NEXUS_2_REST_API + "?g=" + groupId + "&a=" + packageName + "&v=" + packageVersion + "&r=" + repo + "&p=" + format
     try {
-        log.info("searchNexusQuery: ${searchNexusQuery}")K
+        log.info("searchNexusQuery: ${searchNexusQuery}")
         checkStatusNexus2(getApiNexusCall(searchNexusQuery), packageName, packageVersion)
     } catch (FileNotFoundException e) {
         log.info("Package ${packageName} with version ${packageVersion} not found in Nexus.")
