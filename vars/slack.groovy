9d697a5d535a7297a573277c07f6bc6bb5156a93
Evgeniy Sakhnyk <Evgeniy.Sakhnyk@nextiva.com>
2018-09-03 07:25:37 -0700

    Merge pull request #217 in REL/pipelines from feature/PIPELINE-56 to develop
    Squashed commit of the following:
    
    commit 4fd0a5700f0e2c4937d5146af7f75045a5181c55
    Author: Evgeniy Sakhnyk <Evgeniy.Sakhnyk@nextiva.com>
    Date:   Mon Sep 3 16:57:33 2018 +0300
    
        use default implementation of slacksend command

--- vars/slack.groovy
+++ vars/slack.groovy
@@ -34,11 +34,7 @@ def prOwnerPrivateMessage(String url) {
 }
 
 def privateMessage(String slackUserId, String message) {
-    log.debug("Message: " + message)
-    def attachments = java.net.URLEncoder.encode(message, "UTF-8")
-    httpRequest contentType: 'APPLICATION_JSON', quiet: !log.isDebug(),
-        consoleLogResponseBody: log.isDebug(), httpMode: 'POST',
-        url: "https://nextivalab.slack.com/api/chat.postMessage?token=${SLACK_BOT_TOKEN}&channel=${slackUserId}&as_user=true&attachments=${attachments}"
+    call(slackUserId, message)
 }
 
 def buildStatusMessageBody() {
