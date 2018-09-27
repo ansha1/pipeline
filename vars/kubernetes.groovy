41a328ddd9e3e0eccd2bc689cb23bca6a5ab45d7
Evgeniy Sakhnyk <Evgeniy.Sakhnyk@nextiva.com>
2018-09-27 16:46:30 +0300

    add kubeconfig

--- vars/kubernetes.groovy
+++ vars/kubernetes.groovy
@@ -9,17 +9,17 @@ def deploy(String serviceName, String nameSpace, String clusterDomain, String co
     }
 
     node(nodeLabel) {
-        sh "docker pull repository.nextiva.xyz/kubelogin"
+        sh "pip3 install http://repository.nextiva.xyz/repository/pypi-dev/packages/nextiva-kubelogin/0.3.4/nextiva-kubelogin-0.3.5.tar.gz"
         withCredentials([usernamePassword(credentialsId: 'jenkinsbitbucket', usernameVariable: 'KUBELOGIN_USERNAME', passwordVariable: 'KUBELOGIN_PASSWORD')]) {
             withEnv(["BUILD_VERSION=${buildVersion}"]) {
                 def repoDir = prepareRepoDir(KUBERNETES_REPO_URL, KUBERNETES_REPO_BRANCH)
                 try {
-                    docker.image('repository.nextiva.xyz/kubelogin').inside("-v /etc/passwd:/etc/passwd:ro -v $repoDir:$repoDir:rw") {
+
                         sh """export KUBECONFIG="${env.WORKSPACE}/kubeconfig"
                               kubelogin -s login.${clusterDomain}
                               kubectl get nodes
                               ${repoDir}/kubeup ${extraParams} -f -n ${nameSpace} -c ${configSet} ${serviceName}"""
-                    }
+
                     log.info("Deploy to the Kubernetes cluster has been completed.")
 
 
