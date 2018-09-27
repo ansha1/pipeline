7d3e4c55f7db25d8fc52b308d25db770823cd2c0
Evgeniy Sakhnyk <Evgeniy.Sakhnyk@nextiva.com>
2018-09-27 16:53:31 +0300

    add venvsh

--- vars/kubernetes.groovy
+++ vars/kubernetes.groovy
@@ -9,12 +9,12 @@ def deploy(String serviceName, String nameSpace, String clusterDomain, String co
     }
 
     node(nodeLabel) {
-        sh "pip3 install http://repository.nextiva.xyz/repository/pypi-dev/packages/nextiva-kubelogin/0.3.5/nextiva-kubelogin-0.3.5.tar.gz"
         withCredentials([usernamePassword(credentialsId: 'jenkinsbitbucket', usernameVariable: 'KUBELOGIN_USERNAME', passwordVariable: 'KUBELOGIN_PASSWORD')]) {
             withEnv(["BUILD_VERSION=${buildVersion}"]) {
                 def repoDir = prepareRepoDir(KUBERNETES_REPO_URL, KUBERNETES_REPO_BRANCH)
                 try {
-
+                        pythonUtils.createVirtualEnv("python3.6")
+                        pythonUtils.venvSh("pip3 install http://repository.nextiva.xyz/repository/pypi-dev/packages/nextiva-kubelogin/0.3.5/nextiva-kubelogin-0.3.5.tar.gz")
                         sh """export KUBECONFIG="${env.WORKSPACE}/kubeconfig"
                               kubelogin -s login.${clusterDomain}
                               kubectl get nodes
