7a08d40e30382186678b4a5cacbc012a350dafb7
Ivan Fishchuk <ivan.fishchuk@nextiva.com>
2018-10-05 18:39:38 +0300

    check bug for checkBug_with_slackPrMessage

--- vars/slack.groovy
+++ vars/slack.groovy
@@ -40,8 +40,7 @@ def privateMessage(String slackUserId, String message) {
     log.info("Message: " + message)
     def attachments = java.net.URLEncoder.encode(message, "UTF-8")
     log.info("attachments: ${attachments}")
-    httpRequest contentType: 'APPLICATION_JSON', quiet: !log.isDebug(),
-            consoleLogResponseBody: log.isDebug(), httpMode: 'POST',
+    httpRequest contentType: 'APPLICATION_JSON', httpMode: 'POST',
             url: "https://nextivalab.slack.com/api/chat.postMessage?token=${SLACK_BOT_TOKEN}&channel=${slackUserId}&as_user=true&attachments=${attachments}"
 }
 
