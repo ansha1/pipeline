2ff76a8dff39252452b76e457ee7c856ffdb2ba8
chaley <chris.haley@nextiva.com>
2018-10-16 13:14:45 -0700

    adding buildRelease Java Util and updating example Java Lib Jenkinsfile

--- src/com/nextiva/JavaUtils.groovy
+++ src/com/nextiva/JavaUtils.groovy
@@ -142,4 +142,22 @@ void buildPublish(String appName, String buildVersion, String environment, Map a
             error("buildPublish fail ${e}")
         }
     }
+
+}
+
+void buildRelease(String appName, String buildVersion, String environment, Map args, String deployVersion) {
+    log.info("Build and Release Java application.")
+    log.info("APP_NAME: ${appName}")
+    log.info("BUILD_VERSION: ${buildVersion}")
+    log.info("DEPLOY_VERSION: ${deployVersion}")
+    log.info("ENV: ${environment}")
+    def deployVersionArg = !deployVersion.isEmpty() ? "-DreleaseVersion=${deployVersion}" : ""
+    def releaseCommand = args.get("releaseCommands", "mvn --batch-mode release:prepare -DskipTests ${deployVersionArg} && mvn --batch-mode release:perform -DskipTests")
+    dir(pathToSrc) {
+        try {
+            sh "${releaseCommand}"
+        } catch (e) {
+            error("buildRelease fail ${e}")
+        }
+    }
 }
