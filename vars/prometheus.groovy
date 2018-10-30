938e8a40cc6dd9a4d94fbcb0d25e2f3434ac9f37
Maksym Vasylets <maksym.vasylets@nextiva.com>
2018-10-30 23:15:58 +0200

    timestamp metrics

--- vars/prometheus.groovy
+++ vars/prometheus.groovy
@@ -26,8 +26,6 @@ def sendSummary(String metricName, def metricValue, Map metricLabels = [:], Stri
 def sendMetric(String instance, String jobName, String metricName, def metricValue, String metricType,
                Map metricLabels = [:], String metricHelpMessage = '') {
 
-    metricValue = System.currentTimeMillis()
-
     String labels = mapToLabelsStr(metricLabels)
     String encodedJobName = urlEncode(jobName)
     String encodedInstance = urlEncode(instance)
