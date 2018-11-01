319a472174bb1ec001b6506ea672afaba5ee5175
Ivan Fishchuk <ivan.fishchuk@nextiva.com>
2018-11-01 17:02:39 +0200

    fix for lockable resource

--- vars/prepareRepoDir.groovy
+++ vars/prepareRepoDir.groovy
@@ -1,7 +1,7 @@
 import static com.nextiva.SharedJobsStaticVars.*
 
 
-def call(String repo, String branch, String sharedLibraryRepoDir=SHARED_LIBRARY_REPO_DIR) {
+def call(String repo, String branch, String sharedLibraryRepoDir = SHARED_LIBRARY_REPO_DIR) {
 
     // get exact repo name from the full URL
     // ssh://git@git.nextiva.xyz:7999/rel/release-management.git -> release-management
@@ -10,15 +10,11 @@ def call(String repo, String branch, String sharedLibraryRepoDir=SHARED_LIBRARY_
     // replace trailing slash in branch name to underscore
     // feature/testbranch -> feature_testbranch
     def formattedBranch = branch.replace('/', '_')
-    
+
     def finalRepoDir = sharedLibraryRepoDir + '/' + formattedRepo + '/' + formattedBranch
-    def lockableResource = "lock_" + NODE_NAME.replace(' ', '_') + "_" + finalRepoDir
-    log.info("lockableResource: " + lockableResource)
-    
-    lock(lockableResource) {
-        dir(finalRepoDir) {
-            git branch: branch, credentialsId: GIT_CHECKOUT_CREDENTIALS, url: repo
-        }
+
+    dir(finalRepoDir) {
+        git branch: branch, credentialsId: GIT_CHECKOUT_CREDENTIALS, url: repo
     }
 
     return finalRepoDir
