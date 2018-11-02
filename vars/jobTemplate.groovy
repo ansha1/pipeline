18838c3bb611c6a8638d4b5530da153b136dbfaf
Ivan Fishchuk <ivan.fishchuk@nextiva.com>
2018-11-02 14:22:31 +0200

    added debug

--- vars/jobTemplate.groovy
+++ vars/jobTemplate.groovy
@@ -251,6 +251,7 @@ def call(body) {
                                 sshagent(credentials: [GIT_CHECKOUT_CREDENTIALS]) {
                                     sh """ssh 192.168.50.161 uname -n
                                           ssh -v 192.168.50.161 uname -n
+                                          sleep 3600
                                     """
 //                                    def repoDir = prepareRepoDir(jobConfig.ansibleRepo, jobConfig.ansibleRepoBranch)
 //                                    runAnsiblePlaybook(repoDir, jobConfig.INVENTORY_PATH, jobConfig.PLAYBOOK_PATH, jobConfig.getAnsibleExtraVars())
