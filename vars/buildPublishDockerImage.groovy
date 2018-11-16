a3753a5c2dce48c99b5708a6e360c53392e1409e
Oleksandr Kutsenko <Oleksandr.Kutsenko@nextiva.com>
2018-11-16 14:25:32 +0200

    add tenable push

--- vars/buildPublishDockerImage.groovy
+++ vars/buildPublishDockerImage.groovy
@@ -3,6 +3,7 @@ import static com.nextiva.SharedJobsStaticVars.*
 
 def call(String appName, String buildVersion, String deployEnvironment = 'docker', String extraPath = null) {
     def buildLocation = ''
+    def customImage
 
     if ( extraPath ) {
         log.info("We are going to build docker image within " + extraPath)
@@ -16,11 +17,17 @@ def call(String appName, String buildVersion, String deployEnvironment = 'docker
         generateBuildProperties(deployEnvironment, buildVersion, env.JOB_NAME)
     }
 
-    docker.withRegistry(DOCKER_REGISTRY_URL, DOCKER_REGISTRY_CREDENTIALS_ID) {
-        def customImage = docker.build("${appName}:${buildVersion}", "-f ${buildLocation}/Dockerfile --build-arg build_version=${buildVersion} ${buildLocation}")
+    docker.withRegistry(NEXTIVA_DOCKER_REGISTRY_URL, NEXTIVA_DOCKER_REGISTRY_CREDENTIALS_ID) {
+        customImage = docker.build("${appName}:${buildVersion}", "-f ${buildLocation}/Dockerfile --build-arg build_version=${buildVersion} ${buildLocation}")
         customImage.push()
         customImage.push("latest")
-
-        sh "docker rmi ${customImage.id} ${DOCKER_REGISTRY}/${customImage.id} ${DOCKER_REGISTRY}/${appName}:latest"
     }
+
+    docker.withRegistry(TENABLE_DOCKER_REGISTRY_URL, TENABLE_DOCKER_REGISTRY_CREDENTIALS_ID) {
+        customImage.tag("${TENABLE_DOCKER_REGISTRY}/${appName}:${buildVersion}")
+        customImage.push("${TENABLE_DOCKER_REGISTRY}/${appName}:${buildVersion}")
+    }
+
+    log.debug("Removing Old Images")
+    sh "docker rmi ${customImage.id} ${NEXTIVA_DOCKER_REGISTRY}/${customImage.id} ${TENABLE_DOCKER_REGISTRY}/${customImage.id} ${NEXTIVA_DOCKER_REGISTRY}/${appName}:latest"
 }
