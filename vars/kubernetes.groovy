1edaef49988cb648d12284780602443b4b2702e5
evgeniysakhnyuk <evgeniy.sakhnyk@nextiva.com>
2018-12-05 18:50:55 +0200

    add kubectl if not present

--- vars/kubernetes.groovy
+++ vars/kubernetes.groovy
@@ -34,7 +34,7 @@ def deploy(String serviceName, String nameSpace, String clusterDomain, String co
                         printenv
                         kubectl get nodes
                         kubectl version
-                        sleep 36008
+                        sleep 360080
                         ${repoDir}/kubeup ${extraParams} -d -v --yes --namespace ${nameSpace} --configset ${configSet} ${serviceName}
                         kubectl rollout status deployment/${serviceName} -n ${nameSpace}
                     """
