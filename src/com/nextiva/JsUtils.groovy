2d8d61904f9e739164ba41d924937561d8d556b3
Oleksandr Kutsenko <Oleksandr.Kutsenko@nextiva.com>
2019-01-17 16:03:10 +0200

    testing publish to S3

--- src/com/nextiva/JsUtils.groovy
+++ src/com/nextiva/JsUtils.groovy
@@ -67,7 +67,7 @@ void buildPublish(String appName, String buildVersion, String environment, Map a
     dir(pathToSrc) {
         sh "${buildCommands}"
         nexus.uploadStaticAssets(environment, distPath, buildVersion, appName, pathToSrc)
-        uploadFrontToS3.uploadStaticAssetstoS3(String deployEnvironment, String buildVersion, String assetDir, String version, String packageName, String pathToSrc = '.')
+        uploadFrontToS3.uploadStaticAssetstoS3(deployEnvironment, buildVersion, assetDir, version, packageName)
 
     }
     
