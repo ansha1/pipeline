6dc3c89bf61888957ad2957cb1548e62a7671d58
Oleksandr Kutsenko <Oleksandr.Kutsenko@nextiva.com>
2019-01-17 18:09:50 +0200

    refactor variable name

--- vars/uploadFrontToS3.groovy
+++ vars/uploadFrontToS3.groovy
@@ -4,11 +4,11 @@ def call(String appName, String buildVersion, String environment, Map args, Stri
     withAWS(credentials:'nextiva.io', region:'us-west-2') {
         def jobName = "${env.JOB_NAME}"
         def assetDir = args.get('distPath', 'dist/static')
+        def buildCommands = args.get('buildCommands', "export OUTPUT_PATH=${assetDir} && npm install && npm run dist")
         def pathToBuildPropertiesFile = "${pathToSrc}/${BUILD_PROPERTIES_FILENAME}"
         def S3DevBucketName = "static-assests-test"
         def S3ProdBucketName = "static-assets-production.nextiva.io"
         def S3BucketName = ""
-        def buildCommands = args.get('buildCommands', "export OUTPUT_PATH=${assetDir} && npm install && npm run dist")
 
         if (${env.BRANCH_NAME} == "master") {
             S3BucketName = S3DevBucketName
@@ -16,7 +16,7 @@ def call(String appName, String buildVersion, String environment, Map args, Stri
             S3BucketName = S3ProdBucketName
         }
         
-        if (deployEnvironment in LIST_OF_ENVS) {
+        if (environment in LIST_OF_ENVS) {
             generateBuildProperties(environment, buildVersion, jobName)
         } else {
             throw new IllegalArgumentException("Provided env ${environment} is not in the list ${LIST_OF_ENVS}")
