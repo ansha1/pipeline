18aa90b49ddbdd1ba05bdf586355966228a73997
Oleksandr Kutsenko <Oleksandr.Kutsenko@nextiva.com>
2019-01-17 16:40:49 +0200

    testing publish to S3

--- vars/uploadFrontToS3.groovy
+++ vars/uploadFrontToS3.groovy
@@ -6,8 +6,9 @@ def call(String deployEnvironment, String assetDir, String version, String packa
 def uploadStaticAssetstoS3(String environment, String buildVersion, String appName, String assetDir, String pathToSrc) {
     withAWS(credentials:'nextiva.io', region:'us-west-2') {
         def jobName = "${env.JOB_NAME}"
-        def S3DevBucketName = "static-assets-test"
+        def S3DevBucketName = "static-assests-test"
         def S3ProdBucketName = "static-assets-production"
         s3Upload(file:"${assetDir}", bucket:"${S3DevBucketName}", path:"${appName}/${buildVersion}/")
+        s3Upload(file:"${pathToSrc}/build.properties", bucket:"${S3DevBucketName}", path:"${appName}/${buildVersion}/build.properties")
     }
 }
