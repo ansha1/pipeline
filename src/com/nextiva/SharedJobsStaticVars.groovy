0cfd611ac41cc4a3ff57d9d6267ddd4a26938142
Oleksandr Kutsenko <Oleksandr.Kutsenko@nextiva.com>
2019-01-18 14:31:05 +0200

    refactor prod bucket name

--- src/com/nextiva/SharedJobsStaticVars.groovy
+++ src/com/nextiva/SharedJobsStaticVars.groovy
@@ -62,6 +62,6 @@ class SharedJobsStaticVars {
     static final PROMETHEUS_BUILD_FINISHED_METRIC = 0
     static final DEFAULT_VERACODE_APPLICATION_SCOPE = 'NextOS Platform (CRM)'
     static final JENKINS_BOT_URL = 'https://jenkins-bot.tooling.nextiva.io'
-    static final S3_PRODUCTION_BUCKET_NAME = 'static-assets-production.nextiva.io'
+    static final S3_PRODUCTION_BUCKET_NAME = 'static-assets-prod.nextiva.io'
     static final S3_DEV_BUCKET_NAME = 'static-assets-dev.nextiva.io'
 }
