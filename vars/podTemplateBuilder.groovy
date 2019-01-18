c065fc4038f0affce2eed9be46826584cfe0172a
evgeniysakhnyuk <evgeniy.sakhnyk@nextiva.com>
2019-01-18 13:26:11 +0200

    add podtemplate manifests

--- vars/podTemplateBuilder.groovy
+++ vars/podTemplateBuilder.groovy
@@ -6,12 +6,17 @@ def call(body) {
 
 
     podTemplate(label: label, inheritFrom: 'parent', namespace: 'jenkins', workingDir: '/home/jenkins',
-            containers: [containerTemplate(name: 'build', image: "nginx", command: 'cat', ttyEnabled: true)],
+            containers: [containerTemplate(name: 'build', image: image, command: 'cat', ttyEnabled: true)],
             volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock'),
                       hostPathVolume(hostPath: '/opt/m2cache', mountPath: '/opt/m2cache'),
                       hostPathVolume(hostPath: '/opt/npmcache', mountPath: '/opt/npmcache'),
                       hostPathVolume(hostPath: '/opt/yarncache', mountPath: '/opt/yarncache')]) {
         body.call(label)
     }
+}
+
+
+def build(String image){
+    image = this.image
     return this
 }
