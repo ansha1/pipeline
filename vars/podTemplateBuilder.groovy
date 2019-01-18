fb67b2a68632088ac8b67b3b0542ac3330ed0dbd
evgeniysakhnyuk <evgeniy.sakhnyk@nextiva.com>
2019-01-18 13:16:46 +0200

    add podtemplate manifests

--- vars/podTemplateBuilder.groovy
+++ vars/podTemplateBuilder.groovy
@@ -1,2 +1,18 @@
 
- (jobConfig)
+def call(body, String image) {
+    def label= "slave-${UUID.randomUUID().toString()}"
+    parentPodtemplate = libraryResource 'podtemplate/default.yaml'
+
+    podTemplate(label: 'parent', yaml: podfromyaml) {}
+
+
+    podTemplate(label: label, inheritFrom: 'parent', namespace: 'jenkins', workingDir: '/home/jenkins',
+            containers: [containerTemplate(name: 'build', image: image, command: 'cat', ttyEnabled: true)],
+            volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock'),
+                      hostPathVolume(hostPath: '/opt/m2cache', mountPath: '/opt/m2cache'),
+                      hostPathVolume(hostPath: '/opt/npmcache', mountPath: '/opt/npmcache'),
+                      hostPathVolume(hostPath: '/opt/yarncache', mountPath: '/opt/yarncache')]) {
+        body.call(label)
+    }
+    return this
+}
