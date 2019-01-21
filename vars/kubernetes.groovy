df6b18c1914f6b62e5081b9f333facbe0fb772b1
Evgeniy Sakhnyk <Evgeniy.Sakhnyk@nextiva.com>
2019-01-21 07:40:20 -0700

    Merge pull request #290 in REL/pipelines from hotfix/1.14.1 to master
    Squashed commit of the following:
    
    commit abb33a285a535c0d6129c743d7d69614b1cd91d0
    Author: evgeniysakhnyuk <evgeniy.sakhnyk@nextiva.com>
    Date:   Mon Jan 21 16:11:28 2019 +0200
    
        add sleep to avoid failures when deployment doesn't exist yet PIPELINE-93
    
    commit 761d1ba5012da74ab6e65e7c35e2ec050d92d7cd
    Author: evgeniysakhnyuk <evgeniy.sakhnyk@nextiva.com>
    Date:   Mon Jan 21 15:45:30 2019 +0200
    
        fix spelling
    
    commit 9d80307f82928fd99f9ccf6af737c256d79d5fd3
    Author: evgeniysakhnyuk <evgeniy.sakhnyk@nextiva.com>
    Date:   Mon Jan 21 15:40:42 2019 +0200
    
        PIPELINE-95 more clear kubectl rollout fail message, make the build unstable if rollout fail

--- vars/kubernetes.groovy
+++ vars/kubernetes.groovy
@@ -13,10 +13,10 @@ def deploy(String serviceName, String nameSpace, String clusterDomain, String bu
         extraParams = "-v"
     }
 
-    try{
+    try {
         log.info("Ensure that kubectl installed")
         sh "kubectl version --client=true"
-    }catch(e){
+    } catch (e) {
         log.info("Going to install latest stable kubectl")
         sh """
             curl -LO https://storage.googleapis.com/kubernetes-release/release/\$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
@@ -39,13 +39,21 @@ def deploy(String serviceName, String nameSpace, String clusterDomain, String bu
                         ${k8sEnv}/bin/kubelogin -s login.${clusterDomain}
                         kubectl get nodes
                         ${repoDir}/kubeup ${extraParams} --yes --namespace ${nameSpace} --configset ${configSet} ${serviceName}
-                        kubectl rollout status deployment/${serviceName} -n ${nameSpace}
                     """
                 log.info("Deploy to the Kubernetes cluster has been completed.")
             } catch (e) {
                 log.warning("Deploy to the Kubernetes failed!")
                 log.warning(e)
                 error("Deploy to the Kubernetes failed! $e")
+            }
+            sleep 15 // add sleep to avoid failures when deployment doesn't exist yet PIPELINE-93
+            try {
+                sh "kubectl rollout status deployment/${serviceName} -n ${nameSpace}"
+            } catch (e) {
+                log.warning("kubectl rollout status is failed!")
+                log.warning("Ensure that your APP_NAME variable in the Jenkinsfile and metadata.name in app-operator manifest are the same")
+                log.warning(e)
+                currentBuild.rawBuild.result = Result.UNSTABLE
             }
         }
     }
