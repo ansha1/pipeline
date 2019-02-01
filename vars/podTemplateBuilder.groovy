85d9da369533603fad94eb88811d1d20efd403e6
evgeniysakhnyuk <evgeniy.sakhnyk@nextiva.com>
2019-02-01 18:01:42 +0200

    save k8s slaves config manifests in the shared library

--- vars/podTemplateBuilder.groovy
+++ vars/podTemplateBuilder.groovy
@@ -48,7 +48,7 @@ def build(Map podTemplateConfiguration) {
     this.buildNumToKeepStr = podTemplateConfiguration.get("buildNumToKeepStr", "5")
     this.jobTimeoutMinutes = podTemplateConfiguration.get("jobTimeoutMinutes", "60")
     this.paramlist = podTemplateConfiguration.get("paramlist", []) + [booleanParam(name: 'DEBUG', description: 'Enable DEBUG mode with extended output', defaultValue: false)]
-    this.propertiesList = [parameters(paramlist), buildDiscarder(logRotator(daysToKeepStr: buildDaysToKeepStr, numToKeepStr: buildNumToKeepStr)), authorizationMatrix(inheritanceStrategy: nonInheriting(), permissions: securityPermissions)]
+    this.propertiesList = [parameters(paramlist), buildDiscarder(logRotator(daysToKeepStr: this.buildDaysToKeepStr, numToKeepStr: this.buildNumToKeepStr)), authorizationMatrix(inheritanceStrategy: nonInheriting(), permissions: this.securityPermissions)]
     this.isDisableConcurrentBuildsEnabled = podTemplateConfiguration.get("disableConcurrentBuilds", true)
     if (this.isDisableConcurrentBuildsEnabled) {
         this.propertiesList += [disableConcurrentBuilds()]
