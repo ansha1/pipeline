a4812bf8961da3d3ae75bd4c50325c89d285b27b
evgeniysakhnyuk <evgeniy.sakhnyk@nextiva.com>
2019-02-01 13:01:12 +0200

    save k8s slaves config manifests in the shared library

--- vars/podTemplateBuilder.groovy
+++ vars/podTemplateBuilder.groovy
@@ -18,11 +18,16 @@ def call(body) {
 
             node(label) {
                 properties([
-                        buildDiscarder(logRotator(daysToKeepStr: '3', numToKeepStr: '10')),
+                        buildDiscarder(logRotator(daysToKeepStr: buildDaysToKeepStr, numToKeepStr: buildNumToKeepStr)),
+                        parameters([
+//                                string(name: 'submodule', defaultValue: ''),
+//                                string(name: 'submodule_branch', defaultValue: ''),
+//                                string(name: 'commit_sha', defaultValue: ''),
+                        ])
                 ])
                 timestamps {
                     ansiColor('xterm') {
-                        timeout(time: 50, unit: 'MINUTES') {
+                        timeout(time: jobTimeoutMinutes, unit: 'MINUTES') {
                             body.call()
                         }
                     }
@@ -37,6 +42,9 @@ def build(Map podTemplateConfiguration) {
     this.image = podTemplateConfiguration.get("image")
     this.resourceRequestCpu = podTemplateConfiguration.get("resourceRequestCpu", "250m")
     this.resourceRequestMemory = podTemplateConfiguration.get("resourceRequestMemory", "1Gi")
+    this.buildDaysToKeepStr = podTemplateConfiguration.get("buildDaysToKeepStr", "3")
+    this.buildNumToKeepStr = podTemplateConfiguration.get("buildNumToKeepStr", "5")
+    this.jobTimeoutMinutes = podTemplateConfiguration.get("jobTimeoutMinutes", "60")
     return this
 }
 
