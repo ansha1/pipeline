eb51a82d99b0b099cf6a6e785c2338f8aa7a6f74
Evgeniy Sakhnyk <Evgeniy.Sakhnyk@nextiva.com>
2019-02-04 07:41:01 -0700

    Merge pull request #306 in REL/pipelines from feature/podTemplateBuilder to develop
    Squashed commit of the following:
    
    commit e43863bfa92418275d8db16dcde5e5e3e09e80e0
    Author: evgeniysakhnyuk <evgeniy.sakhnyk@nextiva.com>
    Date:   Mon Feb 4 15:24:31 2019 +0200
    
        update cache folders
    
    commit 143ba903576637147912676bfcb5c91953dbcdda
    Author: evgeniysakhnyuk <evgeniy.sakhnyk@nextiva.com>
    Date:   Mon Feb 4 14:56:09 2019 +0200
    
        add default  authenticated if auth is empty
        add docs
    
    commit 44d99a5fbfc685306993023fec84abae17cc8095
    Author: evgeniysakhnyuk <evgeniy.sakhnyk@nextiva.com>
    Date:   Mon Feb 4 13:51:54 2019 +0200
    
        add default  authenticated if auth is empty
        add docs
    
    commit 12aaf528d3217d185fc936eac3bbda997ed5cd10
    Author: evgeniysakhnyuk <evgeniy.sakhnyk@nextiva.com>
    Date:   Mon Feb 4 13:13:02 2019 +0200
    
        create pod template builder
    
    commit cf4a53f678b594476dff102aabf156beb262420d
    Author: evgeniysakhnyuk <evgeniy.sakhnyk@nextiva.com>
    Date:   Fri Feb 1 18:39:01 2019 +0200
    
        create pod template builder

---
+++ vars/podTemplateBuilder.groovy
@@ -0,0 +1,67 @@
+def call(body) {
+    def label = "${slaveName}-${UUID.randomUUID().toString()}"
+    parentPodtemplate = libraryResource 'podtemplate/default.yaml'
+
+    podTemplate(label: 'parent', yaml: parentPodtemplate) {
+        podTemplate(label: label, workingDir: '/home/jenkins',
+                containers: [containerTemplate(name: 'build', image: image, command: 'cat', ttyEnabled: true,
+                        resourceRequestCpu: resourceRequestCpu,
+                        resourceRequestMemory: resourceRequestMemory,
+                        envVars: [
+                                envVar(key: 'CYPRESS_CACHE_FOLDER', value: '/opt/cypress_cache'),
+                                envVar(key: 'YARN_CACHE_FOLDER', value: '/opt/yarn_cache'),
+                                envVar(key: 'CYPRESS_CACHE_FOLDER', value: '/opt/cypress_cache'),
+                                envVar(key: 'npm_config_cache', value: '/opt/npmcache')
+                        ],)],
+                volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock'),
+                          hostPathVolume(hostPath: '/opt/m2cache', mountPath: '/home/jenkins/.m2'),
+                          hostPathVolume(hostPath: '/opt/npmcache', mountPath: '/opt/npmcache'),
+                          hostPathVolume(hostPath: '/opt/cypress_cache', mountPath: '/opt/cypress_cache'),
+                          hostPathVolume(hostPath: '/opt/yarncache', mountPath: '/opt/yarncache')]) {
+
+            timestamps {
+                ansiColor('xterm') {
+                    timeout(time: jobTimeoutMinutes, unit: 'MINUTES') {
+
+                        node(label) {
+                            properties(propertiesList)
+                            body.call()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+def build(Map podTemplateConfiguration) {
+
+    this.slaveName = podTemplateConfiguration.get("slaveName", "slave")
+    this.image = podTemplateConfiguration.get("image")
+    this.resourceRequestCpu = podTemplateConfiguration.get("resourceRequestCpu", "250m")
+    this.resourceRequestMemory = podTemplateConfiguration.get("resourceRequestMemory", "1Gi")
+    this.buildDaysToKeepStr = podTemplateConfiguration.get("buildDaysToKeepStr", "3")
+    this.buildNumToKeepStr = podTemplateConfiguration.get("buildNumToKeepStr", "5")
+    this.jobTimeoutMinutes = podTemplateConfiguration.get("jobTimeoutMinutes", "60")
+    this.paramlist = podTemplateConfiguration.get("paramlist", []) + [booleanParam(name: 'DEBUG', description: 'Enable DEBUG mode with extended output', defaultValue: false)]
+    this.authMap = podTemplateConfiguration.get("auth", [:])
+    this.allowedUsers = this.authMap.get(env.BRANCH_NAME, ["authenticated"])
+    this.securityPermissions = generateSecurityPermissions(this.allowedUsers)
+    this.propertiesList = [parameters(paramlist), buildDiscarder(logRotator(daysToKeepStr: this.buildDaysToKeepStr, numToKeepStr: this.buildNumToKeepStr)), authorizationMatrix(inheritanceStrategy: nonInheriting(), permissions: this.securityPermissions)]
+    this.isDisableConcurrentBuildsEnabled = podTemplateConfiguration.get("disableConcurrentBuilds", true)
+    if (this.isDisableConcurrentBuildsEnabled) {
+        this.propertiesList += [disableConcurrentBuilds()]
+    }
+    return this
+}
+
+
+List<String> generateSecurityPermissions(List<String> allowedUsers) {
+    List<String> basicList = ['hudson.model.Item.Read:authenticated']
+    allowedUsers.each {
+        basicList.add("hudson.model.Item.Build:${it}")
+        basicList.add("hudson.model.Item.Cancel:${it}")
+        basicList.add("hudson.model.Item.Workspace:${it}")
+    }
+    return basicList
+}
