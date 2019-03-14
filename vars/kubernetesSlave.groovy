06d9edcac7ecfcc7d263fea4586a6df9282fc05f
evgeniysakhnyuk <evgeniy.sakhnyk@nextiva.com>
2019-03-14 12:38:56 +0200

    refactor kubernetes slave method

--- vars/kubernetesSlave.groovy
+++ vars/kubernetesSlave.groovy
@@ -4,66 +4,66 @@ import org.csanchez.jenkins.plugins.kubernetes.*
 def call(Map slaveConfig, body) {
 
     //Always generating namespace from JOB_NAME
-    String namespaceName = getNamespaceNameFromString(JOB_NAME)
-    String slaveName = slaveConfig.get("slaveName", "slave")
-    String image = slaveConfig.get("image")
+    def namespaceName = getNamespaceNameFromString(JOB_NAME)
+    def slaveName = slaveConfig.get("slaveName", "slave")
+    def image = slaveConfig.get("image")
     if (image == null) {
         error "Slave image is not defined, please define it in the your slaveConfig"
     }
-    String resourceRequestCpu = slaveConfig.get("resourceRequestCpu", "250m")
-    String resourceRequestMemory = slaveConfig.get("resourceRequestMemory", "1Gi")
-    String buildDaysToKeepStr = slaveConfig.get("buildDaysToKeepStr", "3")
-    String buildNumToKeepStr = slaveConfig.get("buildNumToKeepStr", "5")
-    String jobTimeoutMinutes = slaveConfig.get("jobTimeoutMinutes", "60")
-    List paramlist = slaveConfig.get("paramlist", []) + [booleanParam(name: 'DEBUG', description: 'Enable DEBUG mode with extended output', defaultValue: false)]
+    def resourceRequestCpu = slaveConfig.get("resourceRequestCpu", "250m")
+    def resourceRequestMemory = slaveConfig.get("resourceRequestMemory", "1Gi")
+    def buildDaysToKeepStr = slaveConfig.get("buildDaysToKeepStr", "3")
+    def buildNumToKeepStr = slaveConfig.get("buildNumToKeepStr", "5")
+    def jobTimeoutMinutes = slaveConfig.get("jobTimeoutMinutes", "60")
+    def paramlist = slaveConfig.get("paramlist", []) + [booleanParam(name: 'DEBUG', description: 'Enable DEBUG mode with extended output', defaultValue: false)]
 
     /*jobTriggers could be
     cron('H/15 * * * *')
     upstream(threshold: hudson.model.Result.SUCCESS, upstreamProjects: "surveys-server/dev")
     */
-    List jobTriggers = slaveConfig.get("jobTriggers", [])
-    Map authMap = slaveConfig.get("auth", [:])
-    List allowedUsers = authMap.get(env.BRANCH_NAME, ["authenticated"])
-    List securityPermissions = generateSecurityPermissions(allowedUsers)
-    List propertiesList = [parameters(paramlist),
-                           buildDiscarder(logRotator(daysToKeepStr: buildDaysToKeepStr, numToKeepStr: buildNumToKeepStr)),
-                           authorizationMatrix(inheritanceStrategy: nonInheriting(), permissions: securityPermissions),
-                           disableConcurrentBuilds(),
-                           pipelineTriggers(jobTriggers)]
+    def jobTriggers = slaveConfig.get("jobTriggers", [])
+    def authMap = slaveConfig.get("auth", [:])
+    def allowedUsers = authMap.get(env.BRANCH_NAME, ["authenticated"])
+    def securityPermissions = generateSecurityPermissions(allowedUsers)
+    def propertiesList = [parameters(paramlist),
+                          buildDiscarder(logRotator(daysToKeepStr: buildDaysToKeepStr, numToKeepStr: buildNumToKeepStr)),
+                          authorizationMatrix(inheritanceStrategy: nonInheriting(), permissions: securityPermissions),
+                          disableConcurrentBuilds(),
+                          pipelineTriggers(jobTriggers)]
 
     def label = "${slaveName}-${UUID.randomUUID().toString()}"
-    parentPodtemplateYaml = libraryResource 'podtemplate/default.yaml'
+    def parentPodtemplateYaml = libraryResource 'podtemplate/default.yaml'
 
     //Executing every Jenkins slave in the dedicated namespace
     withNamespace(namespaceName) {
-        podTemplate(label: 'parent', yaml: parentPodtemplateYaml) {
-            podTemplate(label: label, workingDir: '/home/jenkins', namespace: namespaceName,
-                    containers: [containerTemplate(name: 'build', image: image, command: 'cat', ttyEnabled: true,
-                            resourceRequestCpu: resourceRequestCpu,
-                            resourceRequestMemory: resourceRequestMemory,
-                            envVars: [
-                                    envVar(key: 'CYPRESS_CACHE_FOLDER', value: '/opt/cypress_cache'),
-                                    envVar(key: 'YARN_CACHE_FOLDER', value: '/opt/yarn_cache'),
-                                    envVar(key: 'CYPRESS_CACHE_FOLDER', value: '/opt/cypress_cache'),
-                                    envVar(key: 'npm_config_cache', value: '/opt/npmcache'),
-                                    envVar(key: 'M2_LOCAL_REPO', value: '/home/jenkins/.m2repo')
-                            ],)],
-                    volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock'),
-                              hostPathVolume(hostPath: '/opt/m2cache', mountPath: '/home/jenkins/.m2repo'),
-                              hostPathVolume(hostPath: '/opt/npmcache', mountPath: '/opt/npmcache'),
-                              hostPathVolume(hostPath: '/opt/cypress_cache', mountPath: '/opt/cypress_cache'),
-                              hostPathVolume(hostPath: '/opt/yarncache', mountPath: '/opt/yarncache'),
+
+        podTemplate(label: label, yaml: parentPodtemplateYaml, workingDir: '/home/jenkins', namespace: namespaceName,
+                containers: [
+                        containerTemplate(name: 'jnlp', image: "jenkinsci/jnlp-slave:3.27-1-alpine", args: '${computer.jnlpmac} ${computer.name}'),
+                        containerTemplate(name: 'build', image: image, command: 'cat', ttyEnabled: true,
+                                resourceRequestCpu: resourceRequestCpu,
+                                resourceRequestMemory: resourceRequestMemory,
+                                envVars: [
+                                        envVar(key: 'CYPRESS_CACHE_FOLDER', value: '/opt/cypress_cache'),
+                                        envVar(key: 'YARN_CACHE_FOLDER', value: '/opt/yarn_cache'),
+                                        envVar(key: 'CYPRESS_CACHE_FOLDER', value: '/opt/cypress_cache'),
+                                        envVar(key: 'npm_config_cache', value: '/opt/npmcache'),
+                                        envVar(key: 'M2_LOCAL_REPO', value: '/home/jenkins/.m2repo')
+                                ],)],
+                volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock'),
+                          hostPathVolume(hostPath: '/opt/m2cache', mountPath: '/home/jenkins/.m2repo'),
+                          hostPathVolume(hostPath: '/opt/npmcache', mountPath: '/opt/npmcache'),
+                          hostPathVolume(hostPath: '/opt/cypress_cache', mountPath: '/opt/cypress_cache'),
+                          hostPathVolume(hostPath: '/opt/yarncache', mountPath: '/opt/yarncache'),
 //                          secretVolume(mountPath: '/root/.m2', secretName: 'maven-secret')]) {  //TODO: add this secret as shared secret file in all k8s namespaces
-                    ]) {
+                ]) {
 
-                timestamps {
-                    ansiColor('xterm') {
-                        timeout(time: jobTimeoutMinutes, unit: 'MINUTES') {
-
-                            node(label) {
-                                properties(propertiesList)
-                                body.call()
-                            }
+            timestamps {
+                ansiColor('xterm') {
+                    timeout(time: jobTimeoutMinutes, unit: 'MINUTES') {
+                        node(label) {
+                            properties(propertiesList)
+                            body.call()
                         }
                     }
                 }
@@ -71,6 +71,7 @@ def call(Map slaveConfig, body) {
         }
     }
 }
+
 
 List<String> generateSecurityPermissions(List<String> allowedUsers) {
     List<String> basicList = ['hudson.model.Item.Read:authenticated']
@@ -82,6 +83,10 @@ List<String> generateSecurityPermissions(List<String> allowedUsers) {
     return basicList
 }
 
+/*
+This method allow us to run pods in the dedicated namespace.
+The namespace will be deleted after execution
+ */
 def withNamespace(String namespaceName, body) {
     def client = null
     try {
