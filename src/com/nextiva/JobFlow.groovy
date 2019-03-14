8ac5b2c5fa045df9cffb1f4fc793a4a7c44b044d
evgeniysakhnyuk <evgeniy.sakhnyk@nextiva.com>
2019-03-14 17:52:43 +0200

    refactored docker template

--- src/com/nextiva/JobFlow.groovy
+++ src/com/nextiva/JobFlow.groovy
@@ -1,51 +1,158 @@
 package com.nextiva
 
 
-dockerTemplate {
 
-    commonConfig {
-        channelToNotify          // gitflow\trunkbase
-        appName
-        branchingModel
+
+def call(Map config) {
+
+
+
+
+
+
+    stage("Checkout") {
+        container(jnlp) {
+            checkout scm
+        }
+    }
+    stage("Build version verification") {   //if on DEV,develop,master......
+        container(build) {
+            checkversion(config)
+
+            config.check
+            if ()
+        }
+    }
+    stage("Unit testing") {  //Dev release PR    master if trunk base
+        container(build) {
+
+        }
     }
 
-    slaveConfig {
-        extraEnvs
-        slaveName = podTemplateConfiguration.get("slaveName", "slave")
-        buildNamespace = podTemplateConfiguration.get("buildNamespace", "jenkins")
-        image = podTemplateConfiguration.get("image")
-        resourceRequestCpu = podTemplateConfiguration.get("resourceRequestCpu", "250m")
-        resourceRequestMemory = podTemplateConfiguration.get("resourceRequestMemory", "1Gi")
-        buildDaysToKeepStr = podTemplateConfiguration.get("buildDaysToKeepStr", "3")
-        buildNumToKeepStr = podTemplateConfiguration.get("buildNumToKeepStr", "5")
-        jobTimeoutMinutes = podTemplateConfiguration.get("jobTimeoutMinutes", "60")
-        paramlist = podTemplateConfiguration.get("paramlist", []) + [booleanParam(name: 'DEBUG', description: 'Enable DEBUG mode with extended output', defaultValue: false)]
-        authMap = podTemplateConfiguration.get("auth", [:])
-        propertiesList = [parameters(paramlist), buildDiscarder(logRotator(daysToKeepStr: buildDaysToKeepStr, numToKeepStr: buildNumToKeepStr)), authorizationMatrix(inheritanceStrategy: nonInheriting(), permissions: securityPermissions)]
-        isDisableConcurrentBuildsEnabled
+    stage("Sonar analysing") {   //Dev brannh only
+        container(build) {
+
+        }
+    }
+    stage("Build artifact") {
+        container(build) {
+
+        }
+    }
+    stage("Build docker image") {
+        container(build) {
+
+        }
+    }
+    stage("Publish artifact") {
+        container(build) {
+
+        }
+    }
+    stage("Publish docker image") {
+        container(build) {
+
+        }
+    }
+    stage("Veracode security scan") {
+        container(jnlp) {
+
+        }
+    }
+    stage("Tennable security scan") {
+        container(build) {
+
+        }
+    }
+    stage("Integration tests") {  //only on the PR branch
+        container(kubernetes) {
+
+        }
+    }
+    stage("Deploy to the environment") {  //Only on dev/develop/release/master/hotfix
+        container(kubernetes) {
+
+        }
     }
 
-    buildConfig {
-        language
-        pathToSrc
+    stage("Service healthcheck and version validation") {
+        container(jnlp) {
+
+        }
+        //Newrelic info
+        //Prometheus build info
     }
+    stage("Post deploy stage") {
+        container(build) {
 
-    integrationTestsConfig{
-
-        dependenciesList
+        }
+        //Newrelic info
+        //Prometheus build info
     }
+    stage("QA core team tests") {
+        container(jnlp) {
 
-    deployConfig {
-        kubernetesClusterMap
-        Healthcheck
-        postDeployStep{mymethod()}
+        }
+        //Newrelic info
+        //Prometheus build info
     }
+    stage("Send notifications") {
+        container(jnlp) {
 
-    securityConfig{
-        allowedUsersPerBranch dev s
+        }
     }
-
+    return this
 }
+
+
+
+
+
+//dockerTemplate {
+//
+//    commonConfig {
+//        channelToNotify          // gitflow\trunkbase
+//        appName
+//        branchingModel
+//    }
+//
+//    slaveConfig {
+//        extraEnvs
+//        slaveName = podTemplateConfiguration.get("slaveName", "slave")
+//        buildNamespace = podTemplateConfiguration.get("buildNamespace", "jenkins")
+//        image = podTemplateConfiguration.get("image")
+//        resourceRequestCpu = podTemplateConfiguration.get("resourceRequestCpu", "250m")
+//        resourceRequestMemory = podTemplateConfiguration.get("resourceRequestMemory", "1Gi")
+//        buildDaysToKeepStr = podTemplateConfiguration.get("buildDaysToKeepStr", "3")
+//        buildNumToKeepStr = podTemplateConfiguration.get("buildNumToKeepStr", "5")
+//        jobTimeoutMinutes = podTemplateConfiguration.get("jobTimeoutMinutes", "60")
+//        paramlist = podTemplateConfiguration.get("paramlist", []) + [booleanParam(name: 'DEBUG', description: 'Enable DEBUG mode with extended output', defaultValue: false)]
+//        authMap = podTemplateConfiguration.get("auth", [:])
+//        propertiesList = [parameters(paramlist), buildDiscarder(logRotator(daysToKeepStr: buildDaysToKeepStr, numToKeepStr: buildNumToKeepStr)), authorizationMatrix(inheritanceStrategy: nonInheriting(), permissions: securityPermissions)]
+//        isDisableConcurrentBuildsEnabled
+//    }
+//
+//    buildConfig {
+//        language
+//        pathToSrc
+//    }
+//
+//    integrationTestsConfig{
+//
+//        dependenciesList
+//    }
+//
+//    deployConfig {
+//        kubernetesClusterMap
+//        Healthcheck
+//        postDeployStep{mymethod()}
+//    }
+//
+//    securityConfig{
+//        allowedUsersPerBranch dev s
+//    }
+//
+//}
 
 //Context_variables
 //extraenvs
--- vars/dockerTemplate.groovy
+++ vars/dockerTemplate.groovy
@@ -1,102 +1,11 @@
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
 
-
-
-def call(Map Config) {
-
-
-
-
-
-    stage("Checkout") {
-        container(jnlp) {
-            checkout scm
-        }
+    kubernetesSlave(config.slaveConfig) {
+        jobFlow(config.commonConfig)
     }
-    stage("Build version verification") {   //if on DEV,develop,master......
-        container(build) {
-checkversion(config)
-
-            config.check
-            if ()
-        }
-    }
-    stage("Unit testing") {  //Dev release PR    master if trunk base
-        container(build) {
-
-        }
-    }
-
-    stage("Sonar analysing") {   //Dev brannh only
-        container(build) {
-
-        }
-    }
-    stage("Build artifact") {
-        container(build) {
-
-        }
-    }
-    stage("Build docker image") {
-        container(build) {
-
-        }
-    }
-    stage("Publish artifact") {
-        container(build) {
-
-        }
-    }
-    stage("Publish docker image") {
-        container(build) {
-
-        }
-    }
-    stage("Veracode security scan") {
-        container(jnlp) {
-
-        }
-    }
-    stage("Tennable security scan") {
-        container(build) {
-
-        }
-    }
-    stage("Integration tests") {  //only on the PR branch
-        container(kubernetes) {
-
-        }
-    }
-    stage("Deploy to the environment") {  //Only on dev/develop/release/master/hotfix
-        container(kubernetes) {
-
-        }
-    }
-
-    stage("Service healthcheck and version validation") {
-        container(jnlp) {
-
-        }
-        //Newrelic info
-        //Prometheus build info
-    }
-    stage("Post deploy stage") {
-        container(build) {
-
-        }
-        //Newrelic info
-        //Prometheus build info
-    }
-    stage("QA core team tests") {
-        container(jnlp) {
-
-        }
-        //Newrelic info
-        //Prometheus build info
-    }
-    stage("Send notifications") {
-        container(jnlp) {
-
-        }
-    }
-    return this
 }
