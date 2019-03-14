af53ef6906eda6893fa8b2360b6e355ff64cac05
Maksym Vasylets <maksym.vasylets@nextiva.com>
2019-03-14 23:51:36 +0200

    fix

--- vars/kubernetes.groovy
+++ vars/kubernetes.groovy
@@ -95,7 +95,7 @@ def kubectlInstall() {
 
 def kubeup(String serviceName, String configSet, String nameSpace = '', Boolean dryRun = false) {
     String dryRunParam = dryRun ? '--dry-run' : ''
-    String nameSpaceParam = nameSpace ? '' : "--namespace ${nameSpace}"
+    String nameSpaceParam = nameSpace == '' ? '' : "--namespace ${nameSpace}"
 
     sh """
        unset KUBERNETES_SERVICE_HOST
