571de97a065acb48f6ea4ca63cff795302f7c6b4
Oleksandr Kutsenko <Oleksandr.Kutsenko@nextiva.com>
2019-03-27 15:56:51 +0200

    replace "+" -> "-" for buildVersion in s3Upload

--- vars/aws.groovy
+++ vars/aws.groovy
@@ -7,6 +7,7 @@ def uploadFrontToS3(String appName, String buildVersion, String environment, Map
     }
 
     def assetDir = args.get('distPath', 'dist/static')
+    buildVersion = buildVersion.replace('+', '-')
     String S3BucketName = env.BRANCH_NAME.matches(/^(release|hotfix)\/.+$/) ? S3_PUBLIC_BUCKET_NAME : S3_PRIVATE_BUCKET_NAME
 
     withAWS(credentials: AWS_CREDENTIALS, region: AWS_REGION) {
