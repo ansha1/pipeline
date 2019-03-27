22fb88ddf29454cf9b7e2f3a0888c56ff49037f7
oleksandr.kramarenko <oleksandr.kramarenko@nextiva.com>
2019-03-27 16:31:06 +0200

    PIPELINE-124 Better Slack Notifications for Jenkins Builds - Draft

--- src/com/nextiva/slack/MessagesFactory.groovy
+++ src/com/nextiva/slack/MessagesFactory.groovy
@@ -27,7 +27,7 @@ class MessagesFactory implements Serializable {
         message.setBlocks(blocks)
         message.setColor("good")
 
-        return JsonOutput.toJson(ImmutableList.of(message))
+        return JsonOutput.toJson(blocks)
     }
 }
 
