dd60b965578a18c73f9506d122fcc2222a89e3ef
oleksandr.kramarenko <oleksandr.kramarenko@nextiva.com>
2019-03-27 16:28:07 +0200

    PIPELINE-124 Better Slack Notifications for Jenkins Builds - Draft

--- src/com/nextiva/slack/dto/SlackMessage.groovy
+++ src/com/nextiva/slack/dto/SlackMessage.groovy
@@ -3,6 +3,7 @@ package com.nextiva.slack.dto
 import com.nextiva.slack.dto.blocks.Block
 
 class SlackMessage implements Serializable {
+    def text = "some text"
     List<Block> blocks = new ArrayList<>()
     String color //good (green), warning (yellow), danger (red)
 }
