ebc86a69e2c7ca9d41d8a75d3538a1d09d265cf0
oleksandr.kramarenko <oleksandr.kramarenko@nextiva.com>
2019-03-28 16:40:43 +0200

    PIPELINE-124 Better Slack Notifications for Jenkins Builds - Draft

--- src/com/nextiva/slack/MessagesFactory.groovy
+++ src/com/nextiva/slack/MessagesFactory.groovy
@@ -1,10 +1,12 @@
 package com.nextiva.slack
 
+import com.google.common.collect.ImmutableList
 import com.nextiva.slack.dto.SlackMessage
 import com.nextiva.slack.dto.blocks.Block
 import com.nextiva.slack.dto.blocks.Section
 import com.nextiva.slack.dto.composition.Text
 import com.nextiva.slack.dto.blocks.Divider
+import hudson.tasks.test.AbstractTestResultAction
 
 class MessagesFactory implements Serializable {
 
@@ -13,9 +15,17 @@ class MessagesFactory implements Serializable {
 
         blocks.add(new Divider())
 
-        Section section = new Section()
-        section.setText(new Text(getBuildStatusTitle(context)))
-        blocks.add(section)
+        Section title = new Section()
+        title.setText(new Text(getBuildInfoTitle(context)))
+        blocks.add(title)
+
+        Section infoBlocks = new Section()
+        infoBlocks.setFields(ImmutableList.of(
+                new Text(getBuildBranch()),
+                new Text(getStatus()),
+                new Text(getTestResults()))
+        )
+        blocks.add(infoBlocks)
 
         def message = new SlackMessage()
         message.setBlocks(blocks)
@@ -23,7 +33,7 @@ class MessagesFactory implements Serializable {
         return message
     }
 
-    private static getBuildStatusTitle(context) {
+    private static getBuildInfoTitle(context) {
         def mention = ''
         def buildStatus = context.currentBuild.currentResult
         if (buildStatus ==~ "FAILURE" && context.env.BRANCH_NAME ==~ /^(release\/.+|dev|master)$/) {
@@ -32,6 +42,41 @@ class MessagesFactory implements Serializable {
         String jobName = URLDecoder.decode(context.env.JOB_NAME.toString(), 'UTF-8')
         def subject = "Job: ${jobName}, build #${context.env.BUILD_NUMBER}"
         return "${mention}*${subject}*"
+    }
+
+    private static getBuildBranch(context) {
+        return "*Branch:* ${context.env.BRANCH_NAME}"
+    }
+
+    private static getStatus(context) {
+        return "*Status:* ${context.currentBuild.currentResult}"
+    }
+
+    private static getTestResults(context) {
+        def testResultAction = context.currentBuild.rawBuild.getAction(AbstractTestResultAction.class)
+        def summary
+
+        if (testResultAction != null) {
+            def total = testResultAction.getTotalCount()
+            def failed = testResultAction.getFailCount()
+            def skipped = testResultAction.getSkipCount()
+
+            summary = "Passed: " + (total - failed - skipped)
+            summary = summary + (", Failed: " + failed)
+            summary = summary + (", Skipped: " + skipped)
+        } else {
+            summary = "No tests found"
+        }
+        return "*Test results:* ${summary}"
+    }
+
+    private static getCommitAuthor() {
+        def commit = sh(returnStdout: true, script: 'git rev-parse HEAD')
+        return sh(returnStdout: true, script: "git --no-pager show -s --format='%an' ${commit}").trim()
+    }
+
+    private static getLastCommitMessage() {
+        return sh(returnStdout: true, script: 'git log -1 --pretty=%B').trim()
     }
 
 }
