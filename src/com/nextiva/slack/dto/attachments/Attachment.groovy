b65fafeaa8371ad8c40550156ec5ef1187648cd0
oleksandr.kramarenko <oleksandr.kramarenko@nextiva.com>
2019-04-02 12:31:30 +0300

    PIPELINE-124 Better Slack Notifications for Jenkins Builds - Draft

--- src/com/nextiva/slack/dto/attachments/Attachment.groovy
+++ src/com/nextiva/slack/dto/attachments/Attachment.groovy
@@ -4,6 +4,7 @@ class Attachment {
     def title = "title"
     def title_link
     def color = "good"
+    def attachment_type = "default"
     def author_name
     def blocks
     def text = "text"
