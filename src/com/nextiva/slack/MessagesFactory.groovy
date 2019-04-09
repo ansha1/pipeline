8cdb12768b4d427fda07af1475d6f37f90d9950a
oleksandr.kramarenko <oleksandr.kramarenko@nextiva.com>
2019-04-09 19:13:26 +0300

    PIPELINE-124 Better Slack Notifications for Jenkins Builds - Draft

--- src/com/nextiva/slack/MessagesFactory.groovy
+++ src/com/nextiva/slack/MessagesFactory.groovy
@@ -278,7 +278,7 @@ class MessagesFactory implements Serializable {
     private createCommitLink() {
         def commit = context.sh(returnStdout: true, script: 'git rev-parse HEAD')
         //"${createRepositoryUrl()}/commits/${commit}"
-        return "*Last commit:* <${createRepositoryUrl()}/commits/Kappa123|${commit.substring(0, 5)}>"
+        return "*Last commit:* <${createRepositoryUrl()}/commits/83c71410dc9b541bf2d33895c43c6a50b8e48aea|${commit.substring(0, 5)}>"
     }
 
     private createRepositoryUrl() {
