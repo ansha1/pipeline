d1c5098cb2f24cd67320c66fb4e5467e277c2994
oleksandr.kramarenko <oleksandr.kramarenko@nextiva.com>
2019-04-10 15:24:39 +0300

    PIPELINE-124 Better Slack Notifications for Jenkins Builds - Draft

--- src/com/nextiva/slack/MessagesFactory.groovy
+++ src/com/nextiva/slack/MessagesFactory.groovy
@@ -346,7 +346,6 @@ class MessagesFactory implements Serializable {
         def button = new LinkButton()
         button.setText(new Text(text, "plain_text"))
         button.setUrl(link)
-        button.setAction_id("Kappa123")
         return button
     }
 
--- src/com/nextiva/slack/dto/interactive/Button.groovy
+++ src/com/nextiva/slack/dto/interactive/Button.groovy
@@ -2,6 +2,4 @@ package com.nextiva.slack.dto.interactive
 
 abstract class Button implements Serializable {
     def type = "button"
-    def value = "click_me_123"
-    def action_id
 }
