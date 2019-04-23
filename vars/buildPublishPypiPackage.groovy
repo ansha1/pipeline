ea2b778d8451ad23f921cfbccd8717d1c84b2515
Pavlo Nevmerzhytskyi <pavlo.nevmerzhitskyi@nextiva.com>
2019-04-23 17:07:01 +0300

    AN-3631: comment deploy to pypi.nextiva.xyz

--- vars/buildPublishPypiPackage.groovy
+++ vars/buildPublishPypiPackage.groovy
@@ -9,14 +9,14 @@ def call(String extraPath='.', String deployEnvironment=null, String pythonName=
             python setup.py sdist bdist bdist_egg bdist_wheel
         """, false, venvDir)
 
-        log.deprecated('Publishing pypi-package to the old repo - pypi.nextiva.xyz')
-        pythonUtils.venvSh("""
-            pip install devpi-client
-            devpi use http://pypi.nextiva.xyz
-            devpi login root --password iampythonian
-            devpi use root/dev
-            devpi upload --from-dir dist
-        """, false, venvDir)
+        //log.deprecated('Publishing pypi-package to the old repo - pypi.nextiva.xyz')
+        // pythonUtils.venvSh("""
+        //     pip install devpi-client
+        //     devpi use http://pypi.nextiva.xyz
+        //     devpi login root --password iampythonian
+        //     devpi use root/dev
+        //     devpi upload --from-dir dist
+        // """, false, venvDir)
 
         log.info('Publishing pypi-package to Nexus')
 
