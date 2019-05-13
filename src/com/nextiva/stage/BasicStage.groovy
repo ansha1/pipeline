e4ec3576d9f2327e9141139cd585f9c7a7dd818c
evgeniysakhnyuk <evgeniy.sakhnyk@nextiva.com>
2019-05-13 13:29:15 +0300

    add stage factory

--- src/com/nextiva/stage/BasicStage.groovy
+++ src/com/nextiva/stage/BasicStage.groovy
@@ -6,75 +6,11 @@ abstract class BasicStage implements Serializable {
     final protected script
     final protected Map configuration
 
-    static final Map STAGES = ["Checkout"                    : [""],
-                               "VerifyArtifactVersionInNexus": ["deployOnly"    : false,
-                                                                "branchingModel": ["gitflow"   : /^((hotfix|release)\/.+)$/,
-                                                                                   "trunkbased": /^master$/]
-                               ],
-                               "UnitTest"                    : ["deployOnly"    : false,
-                                                                "branchingModel": ["gitflow"   : /^!(master)$/,
-                                                                                   "trunkbased": /^.*$/]
-                               ],
-                               "SonarScan"                   : ["deployOnly"            : false,
-                                                                "isSonarAnalysisEnabled": true,
-                                                                "branchingModel"        : ["gitflow"   : /^(develop|dev)$/,
-                                                                                           "trunkbased": /^master$/]
-                               ],
-                               "IntegrationTest"             : ["deployOnly"               : false,
-                                                                "isIntegrationTestsEnabled": true,
-                                                                "branchingModel"           : ["gitflow"   : /^!(develop|dev|release\/.+|master)$/,
-                                                                                              "trunkbased": /^!(master)$/]
-                               ],
-                               "BuildArtifact"               : ["deployOnly"    : false,
-                                                                "buildArtifact" : true,
-                                                                "branchingModel": ["gitflow"   : /^(dev|develop|hotfix\/.+|release\/.+)$/,
-                                                                                   "trunkbased": /^master$/]
-                               ],
-                               "BuildDockerImage"            : ["deployOnly"      : false,
-                                                                "buildDockerImage": true,
-                                                                "branchingModel"  : ["gitflow"   : /^(dev|develop|hotfix\/.+|release\/.+)$/,
-                                                                                     "trunkbased": /^master$/]
-                               ],
-                               "PublishArtifact"             : ["deployOnly"     : false,
-                                                                "publishArtifact": true,
-                                                                "branchingModel" : ["gitflow"   : /^(dev|develop|hotfix\/.+|release\/.+)$/,
-                                                                                    "trunkbased": /^master$/]
-                               ],
-                               "PublishDockerImage"          : ["deployOnly"        : false,
-                                                                "publishDockerImage": true,
-                                                                "branchingModel"    : ["gitflow"   : /^(dev|develop|hotfix\/.+|release\/.+)$/,
-                                                                                       "trunkbased": /^master$/],
-                               ],
-                               "SecurityScan"                : ["deployOnly"           : false,
-                                                                "isSecurityScanEnabled": true,
-                                                                "branchingModel"       : ["gitflow"   : /^(release|hotfix)\/.+$/,
-                                                                                          "trunkbased": /^!(master)$/]
-                               ],
-                               "Deploy"                      : ["branchingModel": ["gitflow"   : /^(dev|develop|master|release\/.+|hotfix\/.+)$/,
-                                                                                   "trunkbased": /^(master)$/]
-                               ],
-                               "PostDeploy"                  : ["isPostDeployEnabled": true,
-                                                                "branchingModel"     : ["gitflow"   : /^(dev|develop|master|release\/.+|hotfix\/.+)$/,
-                                                                                        "trunkbased": /^(master)$/]
-                               ],
-                               "QACoreTeamTest"              : ["branchingModel": ["gitflow"   : /^(dev|develop|master|release\/.+|hotfix\/.+)$/,
-                                                                                   "trunkbased": /^(master)$/]],
-
-    ]
 
 
     protected BasicStage(Script script, Map configuration) {
         this.script = script
         this.configuration = configuration
-    }
-
-//    BasicStage(script, Map configuration) {
-//        this.script = script
-//        this.configuration = configuration
-//    }
-
-    static BasicStage loadConfiguration(script, configuration) {
-        return Class.forName("com.nextiva.stage.Checkout")
     }
 
 /*
---
+++ src/com/nextiva/stage/StageFactory.groovy
@@ -0,0 +1,70 @@
+package com.nextiva.stage
+
+
+class StageFactory {
+    static Map getStages() {
+        return Stages
+    }
+    static final Map Stages = ["Checkout"                    : [""],
+                               "VerifyArtifactVersionInNexus": ["deployOnly"    : false,
+                                                                "branchingModel": ["gitflow"   : /^((hotfix|release)\/.+)$/,
+                                                                                   "trunkbased": /^master$/]
+                               ],
+                               "UnitTest"                    : ["deployOnly"    : false,
+                                                                "Class"         : UnitTest.class,
+                                                                "branchingModel": ["gitflow"   : /^!(master)$/,
+                                                                                   "trunkbased": /^.*$/]
+                               ],
+                               "SonarScan"                   : ["deployOnly"            : false,
+                                                                "isSonarAnalysisEnabled": true,
+                                                                "branchingModel"        : ["gitflow"   : /^(develop|dev)$/,
+                                                                                           "trunkbased": /^master$/]
+                               ],
+                               "IntegrationTest"             : ["deployOnly"               : false,
+                                                                "isIntegrationTestsEnabled": true,
+                                                                "branchingModel"           : ["gitflow"   : /^!(develop|dev|release\/.+|master)$/,
+                                                                                              "trunkbased": /^!(master)$/]
+                               ],
+                               "BuildArtifact"               : ["deployOnly"    : false,
+                                                                "buildArtifact" : true,
+                                                                "branchingModel": ["gitflow"   : /^(dev|develop|hotfix\/.+|release\/.+)$/,
+                                                                                   "trunkbased": /^master$/]
+                               ],
+                               "BuildDockerImage"            : ["deployOnly"      : false,
+                                                                "buildDockerImage": true,
+                                                                "branchingModel"  : ["gitflow"   : /^(dev|develop|hotfix\/.+|release\/.+)$/,
+                                                                                     "trunkbased": /^master$/]
+                               ],
+                               "PublishArtifact"             : ["deployOnly"     : false,
+                                                                "publishArtifact": true,
+                                                                "branchingModel" : ["gitflow"   : /^(dev|develop|hotfix\/.+|release\/.+)$/,
+                                                                                    "trunkbased": /^master$/]
+                               ],
+                               "PublishDockerImage"          : ["deployOnly"        : false,
+                                                                "publishDockerImage": true,
+                                                                "branchingModel"    : ["gitflow"   : /^(dev|develop|hotfix\/.+|release\/.+)$/,
+                                                                                       "trunkbased": /^master$/],
+                               ],
+                               "SecurityScan"                : ["deployOnly"           : false,
+                                                                "isSecurityScanEnabled": true,
+                                                                "branchingModel"       : ["gitflow"   : /^(release|hotfix)\/.+$/,
+                                                                                          "trunkbased": /^!(master)$/]
+                               ],
+                               "Deploy"                      : ["branchingModel": ["gitflow"   : /^(dev|develop|master|release\/.+|hotfix\/.+)$/,
+                                                                                   "trunkbased": /^(master)$/]
+                               ],
+                               "PostDeploy"                  : ["isPostDeployEnabled": true,
+                                                                "branchingModel"     : ["gitflow"   : /^(dev|develop|master|release\/.+|hotfix\/.+)$/,
+                                                                                        "trunkbased": /^(master)$/]
+                               ],
+                               "QACoreTeamTest"              : ["branchingModel": ["gitflow"   : /^(dev|develop|master|release\/.+|hotfix\/.+)$/,
+                                                                                   "trunkbased": /^(master)$/]],
+
+    ]
+
+
+    static BasicStage getStage(BasicStage stage, Script script, configuration){
+        return  = stage.getDeclaredConstructor(Script, Map).newInstance(this, configuration)
+    }
+
+}
