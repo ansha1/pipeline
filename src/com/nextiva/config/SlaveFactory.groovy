b03afc05403b9c1cde1c1bb202dd641ed1ff6c57
evgeniysakhnyuk <evgeniy.sakhnyk@nextiva.com>
2019-05-30 23:29:15 +0300

    debug

--- src/com/nextiva/config/SlaveFactory.groovy
+++ src/com/nextiva/config/SlaveFactory.groovy
@@ -32,14 +32,14 @@ class SlaveFactory {
         Map toolContainers = [:]
         tools.each { tool, toolConfiguration ->
             Map toolContainerConfiguration = DEFAULT_CONTAINERS.get(tool)
-            (toolContainerConfiguration << toolConfiguration.subMap(["image",
+            toolContainerConfiguration << toolConfiguration.subMap(["image",
                                                                      "resourceRequestCpu",
                                                                      "resourceLimitCpu",
                                                                      "resourceRequestMemory",
                                                                      "resourceLimitMemory",
             ]).findAll {
                 it.value
-            })
+            }
             toolContainers.put(tool, toolContainerConfiguration)
         }
         return toolContainers
