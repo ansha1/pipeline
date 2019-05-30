3740dd9548166af89f410b05dd3ca72469c3cc66
evgeniysakhnyuk <evgeniy.sakhnyk@nextiva.com>
2019-05-30 17:10:46 +0300

    debug

--- src/com/nextiva/config/Config.groovy
+++ src/com/nextiva/config/Config.groovy
@@ -15,9 +15,9 @@ class Config implements Serializable {
 
     Config(Script script, Map pipelineParams) {
         script.log.info("1 complete")
-        this.script = script
+//        this.script = script
         script.log.info("2 complete")
-        this.configuration = pipelineParams
+//        this.configuration = pipelineParams
         script.log.info("preload complete")
 //        validate()
 //        script.log.info("preload complete")
@@ -34,114 +34,114 @@ class Config implements Serializable {
 //        configureSlave()
 //        script.log.info("preload configureSlave() complete")
     }
-    @NonCPS
-    void validate() {
-        // Checking mandatory variables
-        List<String> configurationErrors = []
-
-        //TODO: add default appName generation based on the repository name
-        if (!configuration.containsKey("appName")) {
-            configurationErrors.add("Application Name is undefined. You have to add it in the pipeline  <<LINK_ON_CONFLUENCE>>")
-        }
-        if (!configuration.containsKey("channelToNotify")) {
-            configurationErrors.add("Slack notification channel is undefined. You have to add it in the pipeline  <<LINK_ON_CONFLUENCE>>")
-        }
-        if (!configuration.containsKey("build")) {
-            configurationErrors.add("Build is undefined. You have to add it in the pipeline  <<LINK_ON_CONFLUENCE>>")
-        }
-
-        if (!configurationErrors.isEmpty()) {
-            script.error("Found error(s) in the configuration:\n ${configurationErrors.toString()}")
-        }
-    }
-    @NonCPS
-    void setDefaults() {
-        //Set flags
-        //Use default value, this also creates the key/value pair in the map.
-        //TODO: move branching model(gitflow and trunkbased) to the class or enum
-        configuration.get("branchingModel", "gitflow")
-        configuration.get("jobTimeoutMinutes", "60")
-        configuration.put("isUnitTestEnabled", configuration.build.any {
-            it.containsKey("unitTestCommands")
-        })
-        configuration.put("isIntegrationTestEnabled", configuration.build.any {
-            it.containsKey("integrationTestCommands")
-        })
-        configuration.put("isDeployEnabled", configuration.containsKey("deploy"))
-        configuration.put("isPostDeployEnabled", configuration.deploy?.containsKey("postDeployCommands"))
-        configuration.get("isSecurityScanEnabled", true)
-        configuration.get("isSonarAnalysisEnabled", true)
-        configuration.get("isQACoreTeamTestEnabled", true)
-        configuration.put("branchName", script.env.BRANCH_NAME)
-        configuration.get("extraEnvs", [:])
-
-        //TODO: use new newrelic method
-        //        this.newRelicId = config.get("newRelicIdMap").get(branchName)
-    }
-
-    @NonCPS
-    void configureEnvironment() {
-        if (configuration.get("isDeployEnabled")) {
-            EnvironmentFactory environmentFactory = new EnvironmentFactory(configuration)
-            List<Environment> environmentsToDeploy = environmentFactory.getAvailableEnvironmentsForBranch(configuration.get("branchName"))
-            configuration.put("environmentsToDeploy", environmentsToDeploy)
-        }
-    }
-
-    @NonCPS
-    void setJobParameters() {
-        JobProperties jobProperties = new JobProperties(script, configuration)
-        configuration.put("jobProperties", jobProperties.toMap())
-    }
-
-    @NonCPS
-    void setExtraEnvVariables() {
-        configuration.extraEnvs.each { k, v -> script.env[k] = v }
-    }
-
-    @NonCPS
-    void configureSlave() {
-        SlaveFactory slaveFactory = new SlaveFactory(this)
-        configuration.put("slaveConfiguration", slaveFactory.getSlaveConfiguration())
-    }
-
-    @NonCPS
-    void configureStages() {
-        List<Stage> stages = StageFactory.getStagesFromConfiguration(script, configuration)
-        configuration.put("stages", stages)
-    }
-
-    Map getConfiguration() {
-        return configuration
-    }
-
-    Map getSlaveConfiguration() {
-        return configuration.get("slaveConfiguration")
-    }
-
-    String getJobTimeoutMinutes() {
-        return configuration.get("jobTimeoutMinutes")
-    }
-
-    Map getJenkinsContainer() {
-        return configuration.get("jenkinsContainer", getDEFAULT_CONTAINERS().get("jnlp"))
-    }
-
-    Map getBuildDependencies() {
-        return configuration.get("dependencies")
-    }
-
-    Map getBuildConfiguration() {
-        return configuration.get("build")
-    }
-
-    Map getDeployConfiguration() {
-        return configuration.get("deploy")
-    }
-
-    List<Stage> getStages() {
-        return configuration.get("stages")
-    }
+//    @NonCPS
+//    void validate() {
+//        // Checking mandatory variables
+//        List<String> configurationErrors = []
+//
+//        //TODO: add default appName generation based on the repository name
+//        if (!configuration.containsKey("appName")) {
+//            configurationErrors.add("Application Name is undefined. You have to add it in the pipeline  <<LINK_ON_CONFLUENCE>>")
+//        }
+//        if (!configuration.containsKey("channelToNotify")) {
+//            configurationErrors.add("Slack notification channel is undefined. You have to add it in the pipeline  <<LINK_ON_CONFLUENCE>>")
+//        }
+//        if (!configuration.containsKey("build")) {
+//            configurationErrors.add("Build is undefined. You have to add it in the pipeline  <<LINK_ON_CONFLUENCE>>")
+//        }
+//
+//        if (!configurationErrors.isEmpty()) {
+//            script.error("Found error(s) in the configuration:\n ${configurationErrors.toString()}")
+//        }
+//    }
+//    @NonCPS
+//    void setDefaults() {
+//        //Set flags
+//        //Use default value, this also creates the key/value pair in the map.
+//        //TODO: move branching model(gitflow and trunkbased) to the class or enum
+//        configuration.get("branchingModel", "gitflow")
+//        configuration.get("jobTimeoutMinutes", "60")
+//        configuration.put("isUnitTestEnabled", configuration.build.any {
+//            it.containsKey("unitTestCommands")
+//        })
+//        configuration.put("isIntegrationTestEnabled", configuration.build.any {
+//            it.containsKey("integrationTestCommands")
+//        })
+//        configuration.put("isDeployEnabled", configuration.containsKey("deploy"))
+//        configuration.put("isPostDeployEnabled", configuration.deploy?.containsKey("postDeployCommands"))
+//        configuration.get("isSecurityScanEnabled", true)
+//        configuration.get("isSonarAnalysisEnabled", true)
+//        configuration.get("isQACoreTeamTestEnabled", true)
+//        configuration.put("branchName", script.env.BRANCH_NAME)
+//        configuration.get("extraEnvs", [:])
+//
+//        //TODO: use new newrelic method
+//        //        this.newRelicId = config.get("newRelicIdMap").get(branchName)
+//    }
+//
+//    @NonCPS
+//    void configureEnvironment() {
+//        if (configuration.get("isDeployEnabled")) {
+//            EnvironmentFactory environmentFactory = new EnvironmentFactory(configuration)
+//            List<Environment> environmentsToDeploy = environmentFactory.getAvailableEnvironmentsForBranch(configuration.get("branchName"))
+//            configuration.put("environmentsToDeploy", environmentsToDeploy)
+//        }
+//    }
+//
+//    @NonCPS
+//    void setJobParameters() {
+//        JobProperties jobProperties = new JobProperties(script, configuration)
+//        configuration.put("jobProperties", jobProperties.toMap())
+//    }
+//
+//    @NonCPS
+//    void setExtraEnvVariables() {
+//        configuration.extraEnvs.each { k, v -> script.env[k] = v }
+//    }
+//
+//    @NonCPS
+//    void configureSlave() {
+//        SlaveFactory slaveFactory = new SlaveFactory(this)
+//        configuration.put("slaveConfiguration", slaveFactory.getSlaveConfiguration())
+//    }
+//
+//    @NonCPS
+//    void configureStages() {
+//        List<Stage> stages = StageFactory.getStagesFromConfiguration(script, configuration)
+//        configuration.put("stages", stages)
+//    }
+//
+//    Map getConfiguration() {
+//        return configuration
+//    }
+//
+//    Map getSlaveConfiguration() {
+//        return configuration.get("slaveConfiguration")
+//    }
+//
+//    String getJobTimeoutMinutes() {
+//        return configuration.get("jobTimeoutMinutes")
+//    }
+//
+//    Map getJenkinsContainer() {
+//        return configuration.get("jenkinsContainer", getDEFAULT_CONTAINERS().get("jnlp"))
+//    }
+//
+//    Map getBuildDependencies() {
+//        return configuration.get("dependencies")
+//    }
+//
+//    Map getBuildConfiguration() {
+//        return configuration.get("build")
+//    }
+//
+//    Map getDeployConfiguration() {
+//        return configuration.get("deploy")
+//    }
+//
+//    List<Stage> getStages() {
+//        return configuration.get("stages")
+//    }
 }
 
 
