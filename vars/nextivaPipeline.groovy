c084839d65a4feac2c1334d32b38b353d9ce2bcb
evgeniysakhnyuk <evgeniy.sakhnyk@nextiva.com>
2019-06-06 13:28:04 +0300

    add loglevel param

--- vars/nextivaPipeline.groovy
+++ vars/nextivaPipeline.groovy
@@ -3,7 +3,7 @@ import com.nextiva.stages.stage.Stage
 import com.nextiva.utils.Logger
 
 def call(body) {
-    Logger.init(this)
+    Logger.init(this, env.LOG_LEVEL)
     Logger log = new Logger(this)
     timestamps {
         ansiColor('xterm') {
