7a3226ed3546d08a5c4ab24586858a5583c38ff9
evgeniysakhnyuk <evgeniy.sakhnyk@nextiva.com>
2019-06-06 13:04:29 +0300

    fix kubernetes plugin integration

--- src/com/nextiva/stages/StageFactory.groovy
+++ src/com/nextiva/stages/StageFactory.groovy
@@ -101,7 +101,7 @@ class StageFactory {
             }
         }
         log.debug("======================================================================================")
-        log.debug("Current flow: ", flow.toString())
+        log.debug("Current flow: ", flow)
         return flow
     }
 
