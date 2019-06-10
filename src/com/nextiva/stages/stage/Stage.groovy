bfa933e65bbeefbcde730fb152ee3a8255911ad0
evgeniysakhnyuk <evgeniy.sakhnyk@nextiva.com>
2019-06-10 18:43:47 +0300

    configure regexp

--- src/com/nextiva/stages/stage/Stage.groovy
+++ src/com/nextiva/stages/stage/Stage.groovy
@@ -12,7 +12,6 @@ abstract class Stage implements Serializable {
     protected Stage(Script script, Map configuration) {
         this.script = script
         this.configuration = configuration
-        this.log = new Logger(this)
     }
 
     def execute() {
