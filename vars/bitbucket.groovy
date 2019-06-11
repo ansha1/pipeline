6d676f1459c76409ae7e2b20a6f4aecb989e6af2
Maksym Vasylets <maksym.vasylets@nextiva.com>
2019-06-12 00:47:27 +0300

    simplify

--- vars/bitbucket.groovy
+++ vars/bitbucket.groovy
@@ -175,7 +175,7 @@ static Map<String, String> parseDescription(String description) {
 
 static String descriptionToString(Map<String, String> description) {
 
-    def convertedDescription = ''
+    String convertedDescription = ''
 
     description.entrySet().each { Map.Entry<String, String> entry ->
         if (entry.getKey() == '') {
@@ -189,12 +189,5 @@ static String descriptionToString(Map<String, String> description) {
     }
 
     // PIPELINE-160 - The maximum number of chars in PR description is 32767
-    Integer maxDescriptionSize = 32767
-    String cropMessage = "\n\nThe description is cropped due to reached limitation ..."
-
-    if(convertedDescription.size() > maxDescriptionSize) {
-        convertedDescription = convertedDescription.take(maxDescriptionSize - cropMessage.size()) + cropMessage
-        log.debug("Reached the maximum number of chars in PR description.")
-    }
-    return convertedDescription
+    return convertedDescription.take(32767)
 }
