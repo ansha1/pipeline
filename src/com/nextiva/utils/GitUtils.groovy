12b2230f68136e38fc0b0867915d7991337ef692
evgeniysakhnyuk <evgeniy.sakhnyk@nextiva.com>
2019-06-21 23:15:35 +0300

    add verbose for git clone command

--- src/com/nextiva/utils/GitUtils.groovy
+++ src/com/nextiva/utils/GitUtils.groovy
@@ -1,13 +1,11 @@
 package com.nextiva.utils
 
-import com.cloudbees.groovy.cps.NonCPS
-import jenkins.model.Jenkins
 import static com.nextiva.utils.Utils.shWithOutput
 
 class GitUtils {
 
     static clone(script, String repository, String branch, String folder = "") {
-        return shWithOutput(script, "git clone $repository --branch $branch --single-branch $folder")
+        return shWithOutput(script, "git clone --progress --verbose $repository --branch $branch --single-branch $folder")
     }
 
 
