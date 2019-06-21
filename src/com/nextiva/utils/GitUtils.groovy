bc20b05e3307de46edc2e2ed1453b3b86dbadd3d
evgeniysakhnyuk <evgeniy.sakhnyk@nextiva.com>
2019-06-22 00:02:35 +0300

    fix git clone

--- src/com/nextiva/utils/GitUtils.groovy
+++ src/com/nextiva/utils/GitUtils.groovy
@@ -6,10 +6,10 @@ import static com.nextiva.SharedJobsStaticVars.GIT_CHECKOUT_CREDENTIALS
 class GitUtils {
 
     static clone(script, String repository, String branch, String folder = "") {
-        script.sshagent(credentials: [GIT_CHECKOUT_CREDENTIALS]) {
-//            return result = shWithOutput(script, "git clone --progress --verbose $repository --branch $branch --single-branch $folder")
-            script.sh "git clone --progress --verbose $repository --branch $branch --single-branch $folder"
+//        script.sshagent(credentials: [GIT_CHECKOUT_CREDENTIALS]) {
+        script.container("jnlp") {
+            return shWithOutput(script, "git clone --progress --verbose $repository --branch $branch --single-branch $folder")
+//            script.sh "git clone --progress --verbose $repository --branch $branch --single-branch $folder"
         }
     }
-
 }
