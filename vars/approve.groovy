5a96b99c10dbd6bfe6a23b543c02494b5484736f
Maksym Vasylets <maksym.vasylets@nextiva.com>
2019-06-23 17:15:25 +0300

    tst

--- vars/approve.groovy
+++ vars/approve.groovy
@@ -3,9 +3,11 @@ import com.nextiva.slack.MessagesFactory
 
 def call(String message = 'Should we proceed?', String UserSlackId, List authorizedApprovers = [], Integer minutes = 5) {
     timeout(minutes) {
+        print('111111111')
         def slackMessage = new MessagesFactory(this).buildApproveMessage(message)
-        slack.sendMessage2(UserSlackId, slackMessage)
-
+        print('2222222222')
+        slack.sendMessage(UserSlackId, slackMessage)
+        print('3333333333')
         return input(id: 'Proceed', message: message, ok: 'Approve', submitter: authorizedApprovers.join(","),
                 submitterParameter: 'approver')
     }
