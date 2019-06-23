277437bafbf3109bb2bd3cd74a3b2faa034b719d
Maksym Vasylets <maksym.vasylets@nextiva.com>
2019-06-24 00:06:16 +0300

    tst

--- vars/approve.groovy
+++ vars/approve.groovy
@@ -2,25 +2,23 @@ import com.nextiva.slack.MessagesFactory
 import static com.nextiva.SharedJobsStaticVars.*
 
 
+// slack identifier, should be of the format `<#C12345>`, `<@U12345>`, `<@U12345|user>`, `@user`, `#channel/user` or `#channel`
 def call(String title = 'Should we proceed?', String text = 'Jenkins is waiting for your approval',
          String UserSlackId, String yesText = 'Approve', String noText = 'Decline',
-         List authorizedApprovers = [], Integer minutes = 15) {
+         List authorizedApprovers = []) {
 
-    timeout(minutes) {
+    if(JENKINS_BOT_ENABLE) {
+        String titleLink = "${BUILD_URL}input/"
 
-        if(JENKINS_BOT_ENABLE) {
-            String titleLink = "${BUILD_URL}input/"
+        return bot.getJenkinsApprove(UserSlackId, yesText, noText, title, titleLink, text, titleLink,
+                authorizedApprovers)
+    }
+    else {
+        def slackMessage = new MessagesFactory(this).buildApproveMessage(title, text)
+        slack.sendPrivatMessage(UserSlackId, slackMessage)
 
-            return bot.getJenkinsApprove(UserSlackId, yesText, noText, title, titleLink, text, titleLink,
-                    authorizedApprovers)
-        }
-        else {
-            def slackMessage = new MessagesFactory(this).buildApproveMessage(title, text)
-            slack.sendPrivatMessage(UserSlackId, slackMessage)
-
-            return input(id: 'Proceed', message: text, ok: yesText, submitter: authorizedApprovers.join(","),
-                    submitterParameter: 'approver')
-        }
+        return input(id: 'Proceed', message: text, ok: yesText, submitter: authorizedApprovers.join(","),
+                submitterParameter: 'approver')
     }
 }
 
