b35f475f4dcfad6bf47654af52c351ea4dd6c95b
Anton Doroshenko <anton.doroshenko@nextiva.com>
2019-08-06 15:23:33 +0300

    added isDeployEnabled property to Global singleton

--- src/com/nextiva/config/Global.groovy
+++ src/com/nextiva/config/Global.groovy
@@ -1,11 +1,15 @@
 package com.nextiva.config
 
+import com.nextiva.tools.deploy.DeployTool
+
 // For unknown reason @Singleton annotation did not work well when running on Jenkins
 class Global implements Serializable {
     String appName
     String globalVersion
     String branchName
     String branchingModel
+    DeployTool deployTool
+    Boolean isDeployEnabled = true
 
     private static Global single_instance = null
 
