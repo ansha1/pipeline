a69c735318eeb6174c76e05a15570ef48c066f28
Anton Doroshenko <anton.doroshenko@nextiva.com>
2019-09-11 17:51:28 +0300

    Moved gitflow process steps check into individual class

---
+++ src/test/groovy/com/nextiva/stages/GitflowProcessTest.groovy
@@ -0,0 +1,95 @@
+package com.nextiva.stages
+
+import com.lesfurets.jenkins.unit.BasePipelineTest
+import com.nextiva.SharedJobsStaticVars
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import utils.BranchNames
+import utils.JenkinsScriptsHelper
+import utils.Mocks
+import utils.Validator
+
+import static org.assertj.core.api.Assertions.assertThat
+
+@RunWith(Parameterized.class)
+class GitflowProcessTest extends BasePipelineTest implements Validator, Mocks, JenkinsScriptsHelper {
+
+    @Parameterized.Parameter(0)
+    public String testSetName
+
+    @Parameterized.Parameter(1)
+    public List<String> branchNames
+
+    @Parameterized.Parameter(2)
+    public List<String> expectedSteps
+
+    @Parameterized.Parameters(name = "Test {0}")
+    static Collection<Object[]> data() {
+        return [
+                ["feature", BranchNames.feature.toList(),
+                 ["Checkout", "StartBuildDependencies", "ConfigureProjectVersion", "Build",
+                  "pip: build", "UnitTest", "pip: unitTest", "IntegrationTest", "pip: integrationTest",
+                  "CollectBuildResults", "SendNotifications"]],
+                ["develop", BranchNames.develop.toList(),
+                 ["Checkout", "StartBuildDependencies", "ConfigureProjectVersion", "Build",
+                  "pip: build", "UnitTest", "pip: unitTest", "SonarScan", "IntegrationTest",
+                  "pip: integrationTest", "Publish", "pip: publishArtifact", "Deploy",
+                  "kubeup Deploy: Deploy to dev", "pip: postDeploy", "QACoreTeamTest",
+                  "QA Core Team Tests", "CollectBuildResults", "SendNotifications"]],
+                ["master", BranchNames.master.toList(),
+                 ["Checkout", "ConfigureProjectVersion", "IntegrationTest", "pip: integrationTest",
+                  "Deploy", "kubeup Deploy: Deploy to prod", "kubeup Deploy: Deploy to sales-demo",
+                  "pip: postDeploy", "QACoreTeamTest", "QA Core Team Tests", "CollectBuildResults",
+                  "SendNotifications"]],
+                ["release", BranchNames.release.toList(),
+                 ["Checkout", "StartBuildDependencies", "VerifyArtifactVersionInNexus",
+                  "pip VerifyArtifactVersionInNexus", "docker VerifyArtifactVersionInNexus", "ConfigureProjectVersion",
+                  "Build", "pip: build", "UnitTest", "pip: unitTest", "IntegrationTest", "pip: integrationTest",
+                  "Publish", "pip: publishArtifact", "SecurityScan", "Deploy", "kubeup Deploy: Deploy to qa",
+                  "pip: postDeploy", "QACoreTeamTest", "QA Core Team Tests", "CollectBuildResults",
+                  "SendNotifications"]],
+        ].collect { it as Object[] }
+    }
+
+    @Override
+    @Before
+    void setUp() {
+        scriptRoots += "src/test/jenkins/jobs/nextivaPipeline"
+        super.setUp()
+
+        binding.setVariable 'currentBuild', [result: "SUCCESS", rawBuild: mockObjects.job]
+        binding.setVariable 'WORKSPACE', '/opt/jenkins/workspace/some-workspace'
+        binding.setVariable 'BRANCH_NAME', 'dev'
+        binding.setVariable 'GIT_URL', 'ssh://git@git.nextiva.xyz:7999/~oleksandr.kramarenko/qa_integration.git'
+        binding.setVariable 'params', [
+                deploy_version: '1.0',
+                stack         : 'a'
+        ]
+
+        prepareSharedLib()
+
+        helper.registerAllowedMethod 'fileExists', [String], { s ->
+            return s == SharedJobsStaticVars.BUILD_PROPERTIES_FILENAME
+        }
+        helper.registerAllowedMethod 'isPypiPackageExists', [String, String, String], { return true }
+    }
+
+    @Test
+    void gitflowBranchStepsTest() {
+        branchNames.each { branchName ->
+            helper.callStack = []
+            binding.setVariable 'BRANCH_NAME', branchName
+            Script script = loadScriptHelper("simple_python_app.jenkins")
+            script.env.BRANCH_NAME = branchName
+            runScript(script)
+            assertJobStatusSuccess()
+            assertThat(helper.callStack.findAll {
+                call -> call.methodName == "stage"
+            }.collect {
+                it.args.first().toString()
+            }).describedAs("Branch '$branchName' check").containsExactlyElementsOf(expectedSteps)
+        }
+    }
+}
--- src/test/groovy/vars/NextivaPipelineTest.groovy
+++ src/test/groovy/vars/NextivaPipelineTest.groovy
@@ -3,10 +3,8 @@ package vars
 import com.lesfurets.jenkins.unit.BasePipelineTest
 import com.lesfurets.jenkins.unit.MethodCall
 import com.nextiva.SharedJobsStaticVars
-import com.nextiva.utils.LogLevel
 import org.junit.Before
 import org.junit.Test
-import utils.BranchNames
 import utils.JenkinsScriptsHelper
 import utils.Mocks
 import utils.Validator
@@ -17,28 +15,6 @@ import static org.assertj.core.api.Assertions.assertThat
 
 class NextivaPipelineTest extends BasePipelineTest implements Validator, Mocks, JenkinsScriptsHelper {
 
-    private void branchStepsValidator(Set<String> branchNames, Set<String> requiredSteps) {
-        helper.registerAllowedMethod 'fileExists', [String], { s ->
-            return s == SharedJobsStaticVars.BUILD_PROPERTIES_FILENAME
-        }
-        helper.registerAllowedMethod 'isPypiPackageExists', [String, String, String], {
-            return true
-        }
-        branchNames.each { branchName ->
-            helper.callStack = []
-            binding.setVariable 'BRANCH_NAME', branchName
-            Script script = loadScriptHelper("simple_python_app.jenkins")
-            script.env.BRANCH_NAME = branchName
-            runScript(script)
-            assertJobStatusSuccess()
-            assertThat(helper.callStack.findAll {
-                call -> call.methodName == "stage"
-            }.collect {
-                it.args.first().toString()
-            }).describedAs("Branch '$branchName' check").containsExactlyElementsOf(requiredSteps)
-        }
-    }
-
     @Override
     @Before
     void setUp() {
@@ -46,8 +22,6 @@ class NextivaPipelineTest extends BasePipelineTest implements Validator, Mocks, 
         super.setUp()
 
         binding.setVariable 'currentBuild', [result: "SUCCESS", rawBuild: mockObjects.job]
-//        binding.setVariable 'User', mockObjects.user
-//        binding.setVariable 'NODE_NAME', 'Debian Slave 3'
         binding.setVariable 'WORKSPACE', '/opt/jenkins/workspace/some-workspace'
         binding.setVariable 'BRANCH_NAME', 'dev'
         binding.setVariable 'GIT_URL', 'ssh://git@git.nextiva.xyz:7999/~oleksandr.kramarenko/qa_integration.git'
@@ -71,25 +45,6 @@ class NextivaPipelineTest extends BasePipelineTest implements Validator, Mocks, 
         assertJobStatusSuccess()
     }
 
-    @Test
-    void validate_develop_branch_steps() throws Exception {
-        branchStepsValidator(BranchNames.develop, BranchNames.developSteps)
-    }
-
-    @Test
-    void validate_feature_branch_steps() throws Exception {
-        branchStepsValidator(BranchNames.feature, BranchNames.featureSteps)
-    }
-
-    @Test
-    void validate_release_branch_steps() throws Exception {
-        branchStepsValidator(BranchNames.release, BranchNames.releaseSteps)
-    }
-
-    @Test
-    void validate_master_branch_steps() throws Exception {
-        branchStepsValidator(BranchNames.master, BranchNames.masterSteps)
-    }
 
     @Test
     void fail_if_build_properties_does_not_exists() throws Exception {
